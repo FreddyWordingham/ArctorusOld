a89052daae0beafb7be5653f15c8756943bed377
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-07 17:25:46 +0000

    Added string utility header file.

---
+++ src/utl/string.hpp
@@ -0,0 +1,43 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_STRING_HPP
+#define ARCTORUS_SRC_UTL_STRING_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Timestamp --
+        std::string create_timestamp();
+        std::string create_timestring(const int sec);
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_STRING_HPP
