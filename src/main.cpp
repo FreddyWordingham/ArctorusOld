0e2c5db1db726470a4b75d8eaaaed9667290e0e4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-07 01:51:06 +0000

    Added main.cpp source file.

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/01/2018.
+ */
+
+
+
+//  == INCLUDES ==
+//  -- System --
+#include <iostream>
+
+//  -- General --
+#include "gen/config.hpp"
+
+
+
+//  == NAMESPACE ==
+using namespace arc;
+
+
+
+//  == MAIN ==
+/**
+ *  Main function of the Arctorus program.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main()
+{
+    std::cout << "Hello world!\n";
+    std::cout << "Build information:\n" << BUILD_INFO << "\n";
+
+    return (0);
+}
