65c8cf91f67c05a902d7f3405d1909ba03df7894
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-08 14:44:57 +0000

    Added the vector utility header.

---
+++ src/utl/vector.hpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   08/01/2018.
+ */
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_VECTOR_HPP
+#define ARCTORUS_SRC_UTL_VECTOR_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_VECTOR_HPP
