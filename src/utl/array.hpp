b4836f31462070c9c2f4508c29741d96486faf93
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-08 09:45:36 +0000

    Added the array utility header.

---
+++ src/utl/array.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   08/01/2018.
+ */
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_ARRAY_HPP
+#define ARCTORUS_SRC_UTL_ARRAY_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+#include <array>
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_ARRAY_HPP
