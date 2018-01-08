07390fa0e6216f02e6ab11b7d0a218056599b7e3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-08 11:21:58 +0000

    Added math functions to the array utility header.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 //  == INCLUDES ==
 //  -- System --
+#include <array>
 #include <iostream>
 
 //  -- General --
@@ -17,6 +18,7 @@
 
 //  -- Utility --
 #include "utl/string.hpp"
+#include "utl/array.hpp"
 
 
 
@@ -37,13 +39,36 @@ int main()
     std::cout << "Build information:\n" << BUILD_INFO << "\n";
     std::cout << "Arctorus directory:\n" << ARCTORUS_DIR << "\n";
 
-    std::cout << utl::is_numerical("0.123") << "\n";
-    std::cout << utl::is_numerical("1E23") << "\n";
-    std::cout << utl::is_numerical("-2.0E2") << "\n";
-    std::cout << utl::is_numerical("E3") << "\n";
-    std::cout << utl::is_numerical("") << "\n";
-    std::cout << utl::is_numerical(" ") << "\n";
-    std::cout << utl::is_numerical("  ") << "\n";
+    std::array<double, 7> arr_a({{0.0, -3.0, -7.5, 4.0, M_PI, 1.0/3.0, -1.0/4.0}});
+    std::array<int, 7> arr_b({{0, -3, -7, 4, 3, 0, -2}});
+    std::array<unsigned int, 7> arr_c({{0, 3, 7, 4, 3, 0, 2}});
+
+    std::cout << "\n-\n";
+
+    std::cout << utl::min_index(arr_a) << "\n";
+    std::cout << utl::max_index(arr_a) << "\n";
+    std::cout << utl::min(arr_a) << "\n";
+    std::cout << utl::max(arr_a) << "\n";
+    std::cout << utl::total(arr_a) << "\n";
+    std::cout << utl::magnitude(arr_a) << "\n";
+
+    std::cout << "\n-\n";
+
+    std::cout << utl::min_index(arr_b) << "\n";
+    std::cout << utl::max_index(arr_b) << "\n";
+    std::cout << utl::min(arr_b) << "\n";
+    std::cout << utl::max(arr_b) << "\n";
+    std::cout << utl::total(arr_b) << "\n";
+    std::cout << utl::magnitude(arr_b) << "\n";
+
+    std::cout << "\n-\n";
+
+    std::cout << utl::min_index(arr_c) << "\n";
+    std::cout << utl::max_index(arr_c) << "\n";
+    std::cout << utl::min(arr_c) << "\n";
+    std::cout << utl::max(arr_c) << "\n";
+    std::cout << utl::total(arr_c) << "\n";
+    std::cout << utl::magnitude(arr_c) << "\n";
 
     return (0);
 }
--- src/utl/array.hpp
+++ src/utl/array.hpp
@@ -30,17 +30,170 @@ namespace arc
         //  -- Mathematical --
         template <typename T, size_t N>
         constexpr size_t min_index(const std::array<T, N>& arr);
+        template <typename T, size_t N>
+        constexpr size_t max_index(const std::array<T, N>& arr);
+        template <typename T, size_t N>
+        constexpr T min(const std::array<T, N>& arr);
+        template <typename T, size_t N>
+        constexpr T max(const std::array<T, N>& arr);
+        template <typename T, size_t N>
+        constexpr T total(const std::array<T, N>& arr);
+        template <typename T, size_t N>
+        constexpr double magnitude(const std::array<T, N>& arr);
 
 
 
         //  == FUNCTIONS ==
         //  -- Mathematical --
-
+        /**
+         *  Find the index of the array which holds the smallest element within the array.
+         *  If multiple array elements are equally the smallest, the index of the first will be returned.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to search.
+         *
+         *  @pre    N must not be zero.
+         *
+         *  @return The index of the smallest element within the array.
+         */
         template <typename T, size_t N>
-        constexpr size_t min_index(const std::array<T, N> &arr)
+        constexpr size_t min_index(const std::array<T, N>& arr)
         {
             static_assert(N != 0);
-        };
+
+            size_t index = 0;
+
+            for (size_t i = 1; i < N; ++i)
+            {
+                if (arr[i] < arr[index])
+                {
+                    index = i;
+                }
+            }
+
+            return (index);
+        }
+
+        /**
+         *  Find the index of the array which holds the largest element within the array.
+         *  If multiple array elements are equally the largest, the index of the first will be returned.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to search.
+         *
+         *  @pre    N must not be zero.
+         *
+         *  @return The index of the largest element within the array.
+         */
+        template <typename T, size_t N>
+        constexpr size_t max_index(const std::array<T, N>& arr)
+        {
+            static_assert(N != 0);
+
+            size_t index = 0;
+
+            for (size_t i = 1; i < N; ++i)
+            {
+                if (arr[i] > arr[index])
+                {
+                    index = i;
+                }
+            }
+
+            return (index);
+        }
+
+        /**
+         *  Create a copy of the smallest element within a given array.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to copy the minimum value of.
+         *
+         *  @pre    N must not be zero.
+         *
+         * @return  A copy of the smallest value within the array.
+         */
+        template <typename T, size_t N>
+        constexpr T min(const std::array<T, N>& arr)
+        {
+            static_assert(N != 0);
+
+            return (arr[min_index(arr)]);
+        }
+
+        /**
+         *  Create a copy of the largest element within a given array.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to copy the maximum value of.
+         *
+         *  @pre    N must not be zero.
+         *
+         * @return  A copy of the largest value within the array.
+         */
+        template <typename T, size_t N>
+        constexpr T max(const std::array<T, N>& arr)
+        {
+            static_assert(N != 0);
+
+            return (arr[max_index(arr)]);
+        }
+
+        /**
+         *  Determine the total of all elements stored within a given array.
+         *  Empty arrays are considered to have a total of zero.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to find the total of.
+         *
+         *  @return The total of all elements stored within the array.
+         */
+        template <typename T, size_t N>
+        constexpr T total(const std::array<T, N>& arr)
+        {
+            T total = 0;
+
+            for (size_t i = 0; i < N; ++i)
+            {
+                total += arr[i];
+            }
+
+            return (total);
+        }
+
+        /**
+         *  Determine the magnitude of the given array.
+         *  Empty arrays are considered to have a magnitude of zero.
+         *
+         *  @tparam T   Type stored by the array.
+         *  @tparam N   Size of the array.
+         *
+         *  @param  arr Array to find the magnitude of.
+         *
+         *  @return The magnitude of the array.
+         */
+        template <typename T, size_t N>
+        constexpr double magnitude(const std::array<T, N>& arr)
+        {
+            T squared_total = 0;
+
+            for (size_t i = 0; i < N; ++i)
+            {
+                squared_total += arr[i] * arr[i];
+            }
+
+            return (sqrt(static_cast<double>(squared_total)));
+        }
 
 
 
