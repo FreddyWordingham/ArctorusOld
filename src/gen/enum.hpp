9efceb2c2d66d64d705aa5aca1c12159438bdd9a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-08 09:31:15 +0000

    Added the enum general header file.

---
+++ src/gen/enum.hpp
@@ -0,0 +1,42 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   08/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_ENUM_HPP
+#define ARCTORUS_SRC_GEN_ENUM_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+
+
+
+    //  == ENUMERATIONS ==
+    //  -- Dimensions --
+    /**
+     *  Enumeration of the cartesian dimensions.
+     *  Used for index consistency.
+     */
+    enum cartesian
+    {
+        X,  //! X spatial dimension.
+        Y,  //! Y spatial dimension.
+        Z   //! Z spatial dimension.
+    }
+
+
+
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_ENUM_HPP
