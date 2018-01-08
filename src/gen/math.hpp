b15c6ebc7b1d581c9d18970f86a1f3b259fe75a2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-08 00:43:35 +0000

    Added the math general header file.

---
+++ src/gen/math.hpp
@@ -0,0 +1,77 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   08/01/2018.
+ */
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_MATH_HPP
+#define ARCTORUS_SRC_GEN_MATH_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Power --
+        template <typename T>
+        constexpr T square(const T x);
+        template <typename T>
+        constexpr T cube(const T x);
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Power --
+        /**
+         *  Square a given value.
+         *
+         *  @tparam T   Type being squared.
+         *
+         *  @param  x   Value to be squared.
+         *
+         *  @return The square of the given value.
+         */
+        template <typename T>
+        constexpr T square(const T x)
+        {
+            return (x * x);
+        }
+
+        /**
+         *  Cube a given value.
+         *
+         *  @tparam T   Type being cubed.
+         *
+         *  @param  x   Value to be cubed.
+         *
+         *  @return The cube of the given value.
+         */
+        template <typename T>
+        constexpr T cube(const T x)
+        {
+            return (x * x * x);
+        }
+
+
+
+    } // namespace gen
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_MATH_HPP
