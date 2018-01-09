0ee5bd85e80cf90e2a3953799414d4fdce9a6eee
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-09 14:17:07 +0000

    Added ansi colour general header.

---
+++ src/gen/ansi.hpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_ANSI_HPP
+#define ARCTORUS_SRC_GEN_ANSI_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+
+
+
+    //  == SETTINGS ==
+    //  -- Ansi Colour Codes --
+    constexpr const char* ANSI_COLOUR_RED
+
+
+
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_ANSI_HPP
