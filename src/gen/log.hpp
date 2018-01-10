669b83b61bfe1731385abd1289a038de47fef32b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-10 14:05:23 +0000

    Added the log general header.

---
+++ src/gen/log.hpp
@@ -0,0 +1,27 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   10/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_LOG_HPP
+#define ARCTORUS_SRC_GEN_LOG_HPP
+
+
+
+//  == INCLUDES ==
+//  -- Classes --
+#include "cls/term/logger.hpp"
+
+
+
+//  == MACROS ==
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_LOG_HPP
