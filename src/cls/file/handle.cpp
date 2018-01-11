7f19d1533531cbe8c2d129ca4da169388be1a5c7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-11 13:50:19 +0000

    Added file handle constructor and destructor.

---
+++ src/cls/file/handle.cpp
@@ -0,0 +1,69 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   11/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/file/handle.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace file
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+        /**
+         *  Construct a handle to a given file with the specified mode.
+         *
+         *  @param  init_path   Path to the file being opened.
+         *  @param  mode        Mode to open the file with.
+         */
+        Handle::Handle(const std::string& init_path, const std::fstream::openmode& mode) :
+            path(init_path),
+            filename(utl::strip_path(path)),
+            file(init_file(mode))
+        {
+            // TODO file size warning.
+        }
+
+
+        //  -- Destructors --
+        /**
+         *  Close the open file.
+         *
+         *  @pre    file must be open.
+         *
+         *  @post   file must be closed.
+         */
+        Handle::~Handle()
+        {
+            assert(file.is_open());
+
+            file.close();
+
+            assert(!file.is_open());
+        }
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace file
+} // namespace arc
