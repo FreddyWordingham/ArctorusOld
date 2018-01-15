40b634bddb7d3763a5f295bc8464c8932be7ba05
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-15 14:37:42 +0000

    Added the mathematical matrix class.

---
+++ src/cls/math/mat.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_MATH_MAT_HPP
+#define ARCTORUS_SRC_CLS_MATH_MAT_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace math
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Mathematical matrix class.
+         *  Acts as a mathematical matrix of double values.
+         *  Should not be used for general purpose storage.
+         *
+         *  @tparam N   Number of matrix rows.
+         *  @tparam M   Number of matrix columns.
+         */
+        template <size_t N, size_t M>
+        class Mat
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_MATH_MAT_HPP
