f2b6ed8dcbadbf8a240187c176ad4826bac51c72
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-17 12:22:35 +0000

    Cleaned up main.

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,9 @@
 
 
 //  == INCLUDES ==
-//  -- System --
-#include <cmath>
-
 //  -- General --
-#include "gen/config.hpp"
 #include "gen/log.hpp"
-#include "gen/math.hpp"
 
-//  -- Classes --
-#include "cls/file/handle.hpp"
-
-using namespace arc;
 
 
 //  == MAIN ==
@@ -31,18 +22,6 @@ using namespace arc;
 int main()
 {
     LOG("Hello world!");
-    VERB("Arctorus directory:\n" << arc::config::ARCTORUS_DIR);
-
-    LOG(READ_FILE("../README.md"));
-
-    arc::math::Mat<2, 2> a({{{{4.0, 1.0}}, {{2.0, -3.0}}}});
-    arc::math::Mat<2, 2> b({{{{-1.0, -3.0}}, {{4.0, 2.0}}}});
-
-    VAL(a);
-    VAL(b);
-    VAL(a * b);
-    VAL(math::pos_trans(math::Vec<3>({{1.0, 1.0, 1.0}}), math::Vec<3>({{0.0, 0.0, 0.0}}), math::Vec<3>({{0.0, 0.0, 0.0}})));
-    VAL(math::dir_trans(math::Vec<3>({{1.0, 1.0, 1.0}}), math::Vec<3>({{0.0, 0.0, 0.0}})));
 
     return (0);
 }
