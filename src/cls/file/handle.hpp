32a18b79ccf002e2fb5e7ada12faaba8fcd06f17
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-17 15:19:08 +0000

    Moved settings back into the handler file class header.

--- src/cls/file/handle.hpp
+++ src/cls/file/handle.hpp
@@ -17,9 +17,6 @@
 //  -- System --
 #include <fstream>
 
-//  -- General --
-#include "gen/file.hpp"
-
 
 
 //  == MACROS ==
@@ -33,6 +30,16 @@ namespace arc
 {
     namespace file
     {
+
+
+
+        //  -- Warnings --
+        constexpr const int SIZE_WARNING = 10;  //! [bytes] Warning triggered when opening files larger than this size.
+
+        //  -- Formatting --
+        constexpr const char COMMENT_CHAR = '#';    //! Character used to comment file lines.
+        constexpr const char DELIMIT_CHAR = ',';    //! Character used to delimit file data.
+        constexpr const int  PRINT_WIDTH  = 16;     //! Space allocated to each data print.
 
 
 
