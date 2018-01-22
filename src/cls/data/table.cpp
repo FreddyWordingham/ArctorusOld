bf1d1e1884097e8ca3715e54adc35c1af4641a0d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-22 16:21:04 +0000

    Added the data table class source file.

---
+++ src/cls/data/table.cpp
@@ -0,0 +1,44 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   22/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/data/table.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace data
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+        /**
+         *  Construct an empty data table.
+         */
+        Table::Table() :
+            m_col()
+        {
+        }
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace data
+} // namespace arc
