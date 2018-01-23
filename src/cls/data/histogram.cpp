76884cde82c6e88db1469912e530d3be64f0a7b8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-23 15:27:33 +0000

    Added data histogram source file.

---
+++ src/cls/data/histogram.cpp
@@ -0,0 +1,37 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   23/01/2018.
+ */
+
+
+
+//  == HEADER ==
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace data
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace data
+} // namespace arc
