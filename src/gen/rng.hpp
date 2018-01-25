8c2d9fc157acce2ed83e71d4433fd900af016b72
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 01:31:01 +0000

    Added the rng general header.

---
+++ src/gen/rng.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   25/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_RNG_HPP
+#define ARCTORUS_SRC_GEN_RNG_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace gen
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_RNG_HPP
