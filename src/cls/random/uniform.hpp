449a05c4462ab5d1623bc5efa1291be9b089be4d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 00:53:43 +0000

    Added uniform random class header.

---
+++ src/cls/random/uniform.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   25/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_RANDOM_UNIFORM_HPP
+#define ARCTORUS_SRC_CLS_RANDOM_UNIFORM_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace random
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A sudo-random number generator capable of producing sudo random numbers with uniform probability.
+         *  Primarily used to generate random doubles between zero and unity.
+         */
+        class Uniform
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace random
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_RANDOM_UNIFORM_HPP
