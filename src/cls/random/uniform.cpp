fd36acc6de1fe96a6f86e27d3179da86a75bb5da
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 00:54:49 +0000

    Added uniform random class source file.

---
+++ src/cls/random/uniform.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   25/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/random/uniform.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace random
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace random
+} // namespace arc
