3ff96c9a8647efac0b3d2d18a2f330308cb5b679
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 13:42:33 +0000

    Added the linear interpolator class header.

---
+++ src/cls/interpolator/linear.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   25/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_INTERPOLATOR_LINEAR_HPP
+#define ARCTORUS_SRC_CLS_INTERPOLATOR_LINEAR_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace interpolator
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  TODO
+         */
+        class Linear
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace interpolator
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_INTERPOLATOR_LINEAR_HPP
