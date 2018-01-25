66eee7d1488b0fdbf27e6d3ae7e5795a9148f553
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 01:16:53 +0000

    Added gen_base method.

--- src/cls/random/uniform.cpp
+++ src/cls/random/uniform.cpp
@@ -31,14 +31,14 @@ namespace arc
          *
          *  @return The initalised uniform object.
          */
-        static Uniform& Uniform::get_instance(const uint64_t t_seed)
+        Uniform& Uniform::get_instance(const uint64_t t_seed)
         {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wexit-time-destructors"
             static Uniform s_uni(t_seed);
 #pragma clang diagnostic pop
 
-            return (s_uni)
+            return (s_uni);
         }
 
 
@@ -51,8 +51,8 @@ namespace arc
         Uniform::Uniform(const uint64_t t_seed) :
             m_seed(t_seed),
             m_u(static_cast<uint64_t>(0)),
-            m_u(static_cast<uint64_t>(4101842887655102017)),
-            m_u(static_cast<uint64_t>(1)),
+            m_v(static_cast<uint64_t>(4101842887655102017)),
+            m_w(static_cast<uint64_t>(1))
         {
             init_genaration_variables();
         }
@@ -70,6 +70,30 @@ namespace arc
             gen_base();
             m_w = m_v;
             gen_base();
+        }
+
+
+
+        //  == METHODS ==
+        //  -- Generation --
+        /**
+         *  Generate a base value through bitwise operations.
+         *  All values have an equal probability of generation over time.
+         *
+         *  @return A sudo random base value.
+         */
+        uint64_t Uniform::gen_base()
+        {
+            m_u = m_u + static_cast<uint64_t>(2862933555777941757) + static_cast<uint64_t>(7046029254386353087);
+            m_v ^= m_v >> 17;
+            m_v ^= m_v << 31;
+            m_v ^= m_v >> 8;
+            m_w = static_cast<uint64_t>(4294957665) * (m_w & 0xffffffff) + (m_w >> 32);
+            uint64_t x = m_u ^(m_u << 2);
+            x ^= x >> 35;
+            x ^= x << 4;
+
+            return ((x + m_v) ^ m_w);
         }
 
 
