13990551f517bf800a7c8f79bacd16e87e7d9f53
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-25 13:42:56 +0000

    Added the linear interpolator class source file.

---
+++ src/cls/interpolator/linear.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   25/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/interpolator/linear.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace interpolator
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace interpolator
+} // namespace arc
