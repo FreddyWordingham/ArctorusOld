f0b061ea77806a26c149557fbb6c64cbd1baba81
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 21:45:11 +0000

    Cleaned up main.

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,10 @@
 
 
 //  == INCLUDES ==
+//  -- System --
+
 //  -- General --
 #include "gen/log.hpp"
-
-//  -- Classes --
-#include "cls/data/histogram.hpp"
-#include "cls/random/linear.hpp"
 
 
 
@@ -31,25 +29,6 @@ using namespace arc;
 int main()
 {
     LOG("Hello world!");
-
-    std::vector<double> x, p;
-    for (int            i = -5; i <= 5; ++i)
-    {
-        x.push_back(i);
-        p.push_back(i * i);
-    }
-
-    random::Linear lin(x, p);
-
-
-    data::Histogram hist(-10.0, +10.0, 200);
-
-    for (size_t i = 0; i < 1E6; ++i)
-    {
-        hist(lin());
-    }
-
-    LOG(hist);
 
     return (0);
 }
