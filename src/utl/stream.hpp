b78803e38c1ac10c3e9902d38c67d1c2e7e6d0c1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 23:02:27 +0000

    Added stream utility header.

---
+++ src/utl/stream.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   28/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_STREAM_HPP
+#define ARCTORUS_SRC_UTL_STREAM_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_STREAM_HPP
