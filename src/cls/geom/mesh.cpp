231524f894bd466f7671c6085f409c51dcb61389
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 23:16:09 +0000

    Completed initializer methods.

--- src/cls/geom/mesh.cpp
+++ src/cls/geom/mesh.cpp
@@ -16,6 +16,10 @@
 //  -- System --
 #include "gen/log.hpp"
 
+//  -- Utility --
+#include "utl/stream.hpp"
+
+
 
 //  == NAMESPACE ==
 namespace arc
@@ -95,11 +99,36 @@ namespace arc
             vert_pos.reserve(m_num_vert);
             vert_norm.reserve(m_num_norm);
 
+            // Read in the vertex positions and normals.
             std::stringstream serial_stream(t_serial);
             std::string(line);
             while (std::getline(serial_stream, line))
             {
+                std::stringstream line_stream(line);
+                std::string       word;
+                line_stream >> word;
 
+                if (word == POS_KEYWORD)
+                {
+                    math::Vec<3> pos;
+                    line_stream >> pos[X] >> pos[Y] >> pos[Z];
+
+                    vert_pos.push_back(pos);
+                }
+                else if (word == NORM_KEYWORD)
+                {
+                    math::Vec<3> norm;
+                    line_stream >> norm[X] >> norm[Y] >> norm[Z];
+
+                    norm.normalise();
+
+                    vert_norm.push_back(norm);
+                }
+
+                if (line_stream.fail())
+                {
+                    ERROR("Unable to construct geom::Mesh object.", "Unable to parse serial line: '" << line << "'.");
+                }
             }
 
             // Check the number of vertex positions and normals equals that expected.
@@ -107,7 +136,56 @@ namespace arc
             assert(vert_norm.size() == m_num_norm);
 
 
+            // Read in the triangular faces.
+            utl::rewind(serial_stream);
+            while (std::getline(serial_stream, line))
+            {
+                std::stringstream line_stream(line);
+                std::string       word;
+                line_stream >> word;
 
+                if (word == FACE_KEYWORD)
+                {
+                    std::array<std::string, 3> face;
+                    line_stream >> face[ALPHA] >> face[BETA] >> face[GAMMA];
+
+                    if (line_stream.rdbuf()->in_avail() != 0)
+                    {
+                        ERROR("Unable to construct geom::Mesh object.",
+                              "Non-triangular face located within line: '" << line << "'.");
+                    }
+
+                    std::array<size_t, 3> pos_index{}, norm_index{};
+                    for (size_t           i = 0; i < 3; ++i)
+                    {
+                        const size_t first_slash = face[i].find_first_of('/');
+                        const size_t last_slash  = face[i].find_last_of('/');
+
+                        std::stringstream pos(face[i].substr(0, first_slash));
+                        pos >> pos_index[i];
+                        --pos_index[i];
+
+                        std::stringstream norm(face[i].substr(last_slash + 1));
+                        norm >> norm_index[i];
+                        --norm_index[i];
+
+                        if (pos.fail() || norm.fail())
+                        {
+                            ERROR("Unable to construct geom::Mesh object.",
+                                  "Unable to parse stringified wavefront object line: '" << line << "'.");
+                        }
+                    }
+
+                    r_tri.push_back(
+                        Triangle({{vert_pos[pos_index[ALPHA]], vert_pos[pos_index[BETA]], vert_pos[pos_index[GAMMA]]}},
+                                 {{vert_norm[norm_index[ALPHA]], vert_norm[norm_index[BETA]], vert_norm[norm_index[GAMMA]]}}));
+                }
+
+                if (line_stream.fail())
+                {
+                    ERROR("Unable to construct geom::Mesh object.", "Unable to parse serial line: '" << line << "'.");
+                }
+            }
 
             // Check the number of faces equals that expected.
             assert(r_tri.size() == m_num_tri);
