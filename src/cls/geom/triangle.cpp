3a6617efe3ecc90ee8d631291e6f973165260c33
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 22:04:58 +0000

    Created triangle geometry class source file.

---
+++ src/cls/geom/triangle.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   28/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/geom/triangle.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace geom
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace geom
+} // namespace arc
