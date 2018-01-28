441467d5255eaf7efb733337d1ab3d6c06f18fc0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 22:04:31 +0000

    Created triangle geometry class header.

---
+++ src/cls/geom/triangle.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   28/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GEOM_TRIANGLE_HPP
+#define ARCTORUS_SRC_CLS_GEOM_TRIANGLE_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace geom
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Triangle class used to form triangular meshes.
+         */
+        class Triangle
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GEOM_TRIANGLE_HPP
