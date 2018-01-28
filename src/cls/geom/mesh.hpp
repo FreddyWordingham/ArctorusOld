459a5bec246a51b11f31c54a78cbeb37dfcb678a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-28 22:23:16 +0000

    Added mesh geometry class header.

---
+++ src/cls/geom/mesh.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   28/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GEOM_MESH_HPP
+#define ARCTORUS_SRC_CLS_GEOM_MESH_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace geom
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Triangular mesh class used to form the boundary of objects.
+         */
+        class Mesh
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GEOM_MESH_HPP
