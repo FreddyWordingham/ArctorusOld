4258bfaab7d4331319d3e4f5d67875f9ee07967a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-29 14:37:22 +0000

    Added the graphical camera source file.

---
+++ src/cls/graphical/camera.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   29/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/camera.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace graphical
+} // namespace arc
