f8d4e3397a655f5cd15a86f61c7ee980c7a2f0ed
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-29 12:28:34 +0000

    Added controls header.

---
+++ src/gen/controls.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   29/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_CONTROLS_HPP
+#define ARCTORUS_SRC_GEN_CONTROLS_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace gen
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_CONTROLS_HPP
