5b1c7946318543f55cc38cac363fccbea69eba8a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-29 15:19:18 +0000

    Added graphical fly camera class header.

---
+++ src/cls/graphical/camera/fly.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   29/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_CAMERA_FLY_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_CAMERA_FLY_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/camera.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace camera
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  A specialised camera with fly controls.
+             */
+            class Fly
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace camera
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_CAMERA_FLY_HPP
