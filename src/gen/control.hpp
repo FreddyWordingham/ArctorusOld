4d5839c4186c7dac121bc0b742daa3cfd874ed30
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-30 01:32:15 +0000

    Made constants constexpr.

--- src/gen/control.hpp
+++ src/gen/control.hpp
@@ -29,21 +29,21 @@ namespace arc
 
         //  == SETTINGS ==
         //  -- Window --
-        const int QUIT = GLFW_KEY_ESCAPE;   //! Quit and close the graphical window.
+        constexpr const int QUIT = GLFW_KEY_ESCAPE; //! Quit and close the graphical window.
 
         //  -- Camera --
-        const int DECREASE_SPEED    = GLFW_KEY_LEFT_CONTROL;    //! Decrease camera speed.
-        const int INCREASE_SPEED    = GLFW_KEY_LEFT_SHIFT;      //! Increase camera speed.
-        const int MOVE_CAM_FORWARD  = GLFW_KEY_W;               //! Move the camera forward.
-        const int MOVE_CAM_BACKWARD = GLFW_KEY_S;               //! Move the camera backward.
-        const int MOVE_CAM_LEFT     = GLFW_KEY_A;               //! Move the camera left.
-        const int MOVE_CAM_RIGHT    = GLFW_KEY_D;               //! Move the camera right.
-        const int MOVE_CAM_UP       = GLFW_KEY_E;               //! Move the camera upwards.
-        const int MOVE_CAM_DOWN     = GLFW_KEY_Q;               //! Move the camera downwards.
-        const int ROT_CAM_LEFT      = GLFW_KEY_J;               //! Rotate the camera to the left.
-        const int ROT_CAM_RIGHT     = GLFW_KEY_L;               //! Rotate the camera to the right.
-        const int ROT_CAM_UP        = GLFW_KEY_I;               //! Rotate the camera upwards.
-        const int ROT_CAM_DOWN      = GLFW_KEY_K;               //! Rotate the camera downwards.
+        constexpr const int DECREASE_SPEED    = GLFW_KEY_LEFT_CONTROL;  //! Decrease camera speed.
+        constexpr const int INCREASE_SPEED    = GLFW_KEY_LEFT_SHIFT;    //! Increase camera speed.
+        constexpr const int MOVE_CAM_FORWARD  = GLFW_KEY_W;             //! Move the camera forward.
+        constexpr const int MOVE_CAM_BACKWARD = GLFW_KEY_S;             //! Move the camera backward.
+        constexpr const int MOVE_CAM_LEFT     = GLFW_KEY_A;             //! Move the camera left.
+        constexpr const int MOVE_CAM_RIGHT    = GLFW_KEY_D;             //! Move the camera right.
+        constexpr const int MOVE_CAM_UP       = GLFW_KEY_E;             //! Move the camera upwards.
+        constexpr const int MOVE_CAM_DOWN     = GLFW_KEY_Q;             //! Move the camera downwards.
+        constexpr const int ROT_CAM_LEFT      = GLFW_KEY_J;             //! Rotate the camera to the left.
+        constexpr const int ROT_CAM_RIGHT     = GLFW_KEY_L;             //! Rotate the camera to the right.
+        constexpr const int ROT_CAM_UP        = GLFW_KEY_I;             //! Rotate the camera upwards.
+        constexpr const int ROT_CAM_DOWN      = GLFW_KEY_K;             //! Rotate the camera downwards.
 
 
 
