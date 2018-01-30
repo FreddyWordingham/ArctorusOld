73d17342165526b3617b0d4058a00298867ae14f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-30 12:18:02 +0000

    Added graphical orbit camera class header.

---
+++ src/cls/graphical/camera/orbit.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   30/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_CAMERA_ORBIT_HPP
+#define ARCTORUS_SRC_CLS_CAMERA_ORBIT_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace camera
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A specialised camera with orbit controls.
+         */
+        class Orbit
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace camera
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_CAMERA_ORBIT_HPP
