e7fef768a7a08b243668b04ae84864e08c4dccb9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-30 12:40:58 +0000

    Added constructor.

--- src/cls/graphical/camera/orbit.cpp
+++ src/cls/graphical/camera/orbit.cpp
@@ -8,31 +8,53 @@
 
 
 //  == HEADER ==
-#include "cls/camera/orbit.hpp"
+#include "cls/graphical/camera/orbit.hpp"
 
 
 
 //  == INCLUDES ==
 //  -- System --
 
+//  -- General --
+#include "gen/enum.hpp"
+#include "gen/math.hpp"
+
 
 
 //  == NAMESPACE ==
 namespace arc
 {
-    namespace camera
+    namespace graphical
     {
+        namespace camera
+        {
 
 
 
-        //  == INSTANTIATION ==
-        //  -- Constructors --
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+            /**
+             *  Construct an orbit camera for the graphical window.
+             *
+             *  @param  t_pos           Initial position of the camera.
+             *  @param  t_aspect_ratio  Aspect ratio of the camera.
+             *  @param  t_fov           Camera's field of view in degrees.
+             */
+            Orbit::Orbit(const glm::vec3& t_pos, const float t_aspect_ratio, const float t_fov) :
+                Camera(t_pos, t_aspect_ratio, t_fov),
+                m_azi(atan2f(n_pos[Y], n_pos[X])),
+                m_dec(acosf(n_pos[Z] / sqrtf(math::square(n_pos[X]) + math::square(n_pos[Y]) + math::square(n_pos[Z])))),
+                m_rho(sqrtf(math::square(n_pos[X]) + math::square(n_pos[Y]) + math::square(n_pos[Z]))),
+                m_spin_rate(0.0f)
+            {
+            }
 
 
 
-        //  -- Initialisation --
+            //  -- Initialisation --
 
 
 
-    } // namespace camera
+        } // namespace camera
+    } // namespace graphical
 } // namespace arc
--- src/cls/graphical/camera/orbit.hpp
+++ src/cls/graphical/camera/orbit.hpp
@@ -15,53 +15,56 @@
 
 //  == INCLUDES ==
 //  -- System --
+#include <glm/glm.hpp>
+
+//  -- Classes --
+#include "cls/graphical/camera.hpp"
+
 
 
 
 //  == NAMESPACE ==
 namespace arc
 {
-    namespace camera
+    namespace graphical
     {
-
-
-
-        //  == CLASS ==
-        /**
-         *  A specialised camera with orbit controls.
-         */
-        class Orbit
+        namespace camera
         {
-            //  == FIELDS ==
-          private:
-            //  -- Orientation --
-            float m_azi;    //! Rotation of the camera around the global z-axis.
-            float m_dec;    //! Declination of the camera form the global z-axis.
-            float m_rho;    //! Distance of the camera from the origin.
-
-            //  -- Travel --
-            float m_spin_rate;  //! Rate of continuous rotation of the camera about the global z-axis.
-
-
-            //  == INSTANTIATION ==
-          public:
-            //  -- Constructors --
-
-          private:
-            //  -- Initialisation --
-
-
-            //  == OPERATORS ==
-          private:
-
-
-            //  == METHODS ==
-          private:
-        };
 
 
 
-    } // namespace camera
+            //  == CLASS ==
+            /**
+             *  A specialised camera with orbit controls.
+             */
+            class Orbit : public Camera
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Orientation --
+                float m_azi;    //! Rotation of the camera around the global z-axis.
+                float m_dec;    //! Declination of the camera form the global z-axis.
+                float m_rho;    //! Distance of the camera from the origin.
+
+                //  -- Travel --
+                float m_spin_rate;  //! Rate of continuous rotation of the camera about the global z-axis.
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+                Orbit(const glm::vec3& t_pos, float t_aspect_ratio, float t_fov = 45.0f);
+
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace camera
+    } // namespace graphical
 } // namespace arc
 
 
