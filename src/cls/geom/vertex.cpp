09e106b1759493bc67117dd4bb8e3e927035272f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-31 18:26:47 +0000

    Added geometry vertex source file.

---
+++ src/cls/geom/vertex.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   31/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/geom/vertex.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace geom
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace geom
+} // namespace arc
