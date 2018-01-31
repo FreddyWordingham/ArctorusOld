7d0917adade08215f0688d46055f60a1a5a2d2f0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-31 18:19:27 +0000

    Added the vertex geometry class header.

---
+++ src/cls/geom/vertex.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   31/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GEOM_VERTEX_HPP
+#define ARCTORUS_SRC_CLS_GEOM_VERTEX_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace geom
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Vertices are the base of all geometry.
+         *  Always three dimensional.
+         */
+        class Vertex
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GEOM_VERTEX_HPP
