43de56b2b83789b7ba6cffb9928db78a1a758219
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-01-31 14:55:21 +0000

    Added the diffuse shader graphical source file.

---
+++ src/cls/graphical/shader/diffuse.cpp
@@ -0,0 +1,48 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   31/01/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/shader/diffuse.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+            /**
+             *  Construct an diffuse shader.
+             *
+             *  @param  t_vert_serial   Serialised source code of the vertex shader.
+             *  @param  t_frag_serial   Serialised source code of the fragment shader.
+             */
+            Diffuse::Diffuse(const std::string& t_vert_serial, const std::string& t_frag_serial) :
+                Shader(t_vert_serial, t_frag_serial),
+                m_prop_col(init_uniform(PROP_COL_UNIFORM_NAME))
+            {
+            }
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
