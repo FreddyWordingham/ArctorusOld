2ef9b5dca7de9fb923f2be377d3ed205c1d9d42d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 01:27:32 +0000

    Added constructor method.

--- src/cls/graphical/shader.cpp
+++ src/cls/graphical/shader.cpp
@@ -27,7 +27,17 @@ namespace arc
 
         //  == INSTANTIATION ==
         //  -- Constructors --
-
+        /**
+         *  Construct a shader object from individual geometry, vertex and fragment serialised shader codes.
+         *
+         *  @param  t_geom_serial   Serialised source code of the geometry shader.
+         *  @param  t_vert_serial   Serialised source code of the vertex shader.
+         *  @param  t_frag_serial   Serialised source code of the fragment shader.
+         */
+        Shader::Shader(const std::string& t_geom_serial, const std::string& t_vert_serial, const std::string& t_frag_serial) :
+            m_handle(init_handle(t_geom_serial, t_vert_serial, t_frag_serial))
+        {
+        }
 
 
         //  -- Initialisation --
--- src/cls/graphical/shader.hpp
+++ src/cls/graphical/shader.hpp
@@ -44,6 +44,7 @@ namespace arc
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
+            Shader(const std::string& t_geom_serial, const std::string& t_vert_serial, const std::string& t_frag_serial);
 
           private:
             //  -- Initialisation --
