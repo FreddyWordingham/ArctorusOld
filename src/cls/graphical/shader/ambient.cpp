fccf36109b76d7aba0f7f2b4e58d43c31d2c9b46
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 01:38:56 +0000

    Added ambient graphical shader source file.

---
+++ src/cls/graphical/shader/ambient.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   01/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/shader/ambient.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
