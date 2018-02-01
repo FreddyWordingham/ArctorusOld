d7ba4b825a2dfcf8ca00a543cf01981043c603ce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 00:59:23 +0000

    Added app gui source file.

---
+++ src/cls/app/gui.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   01/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/app/gui.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace app
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace app
+} // namespace arc
