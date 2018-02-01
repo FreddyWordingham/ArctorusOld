e1224ea1a4e65be93cf78e9d02b1f8ffe24fa412
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 01:24:26 +0000

    Added graphical shader source file.

---
+++ src/cls/graphical/shader.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   01/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/shader.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace graphical
+} // namespace arc
