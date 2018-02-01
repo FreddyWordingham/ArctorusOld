21adc0f1c515b11d79160380b6a84d5ac913ca2c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 01:07:38 +0000

    Added graphical scene class header.

---
+++ src/cls/graphical/scene.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   01/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_SCENE_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_SCENE_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Graphical window class.
+         *  Used to visualise three-dimensional simulation data.
+         */
+        class Scene
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_SCENE_HPP
