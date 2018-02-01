e2538fa363be8f736b0966de6eee568a5c983ece
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-01 01:12:11 +0000

    Added graphical scene settings.

--- src/cls/graphical/scene.hpp
+++ src/cls/graphical/scene.hpp
@@ -28,6 +28,28 @@ namespace arc
 
 
 
+        //  == SETTINGS ==
+        //  -- Window --
+        constexpr const int WIDTH  = 1600;                  //! Width of the window in pixels.
+        constexpr const int HEIGHT = 1200;                  //! Height of the window in pixels.
+        constexpr const char* TITLE = "Arctorus";           //! Window title.
+        constexpr const int   AA_SAMPLES         = 4;       //! Number of anti-aliasing samples.
+        constexpr const float CLEAR_COLOUR_RED   = 0.2f;    //! Window red clear colour.
+        constexpr const float CLEAR_COLOUR_GREEN = 0.2f;    //! Window green clear colour.
+        constexpr const float CLEAR_COLOUR_BLUE  = 0.2f;    //! Window blue clear colour.
+        constexpr const float CLEAR_COLOUR_ALPHA = 0.2f;    //! Window alpha clear colour.
+
+        //  -- Camera --
+        constexpr const glm::vec3 INIT_CAM_POS({+10.0f, +10.0f, +10.0f});   //! Initial position of the camera.
+
+        //  -- Shaders --
+        constexpr const char* AMBIENT_VERT_SHADER = "res/shaders/ambient.vert"; //! Path to ambient vertex shader.
+        constexpr const char* AMBIENT_FRAG_SHADER = "res/shaders/ambient.frag"; //! Path to ambient fragment shader.
+        constexpr const char* DIFFUSE_VERT_SHADER = "res/shaders/diffuse.vert"; //! Path to diffuse vertex shader.
+        constexpr const char* DIFFUSE_FRAG_SHADER = "res/shaders/diffuse.frag"; //! Path to diffuse fragment shader.
+
+
+
         //  == CLASS ==
         /**
          *  Graphical window class.
