d1c9088845a8e8a4241f6a7a45927523855f00f1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 02:50:38 +0000

    Added graphical prop class header.

---
+++ src/cls/graphical/prop.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A class containing all information required to draw a prop.
+         */
+        class Prop
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
