256a05e33ac7ce1a24f7f5d891ce8c83dce72ced
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 23:36:34 +0000

    Added graphical light prop source file.

---
+++ src/cls/graphical/prop/light.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/prop/light.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace prop
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace prop
+    } // namespace graphical
+} // namespace arc
