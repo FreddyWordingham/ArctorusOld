bca2daaffe7996e80621f6c0106ad2a8abcdadb1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 04:11:49 +0000

    Reorded shaders.

--- src/cls/graphical/shader/diffuse.cpp
+++ src/cls/graphical/shader/diffuse.cpp
@@ -27,13 +27,13 @@ namespace arc
             /**
              *  Construct a diffuse shader.
              *
-             *  @param  t_geom_serial   Serialised source code of the geometry shader.
              *  @param  t_vert_serial   Serialised source code of the vertex shader.
+             *  @param  t_geom_serial   Serialised source code of the geometry shader.
              *  @param  t_frag_serial   Serialised source code of the fragment shader.
              */
-            Diffuse::Diffuse(const std::string& t_geom_serial, const std::string& t_vert_serial,
+            Diffuse::Diffuse(const std::string& t_vert_serial, const std::string& t_geom_serial,
                              const std::string& t_frag_serial) :
-                Shader(t_geom_serial, t_vert_serial, t_frag_serial),
+                Shader(t_vert_serial, t_geom_serial, t_frag_serial),
                 m_mvp_uni(init_uniform("mvp")),
                 m_view_uni(init_uniform("view")),
                 m_col_uni(init_uniform("col")),
