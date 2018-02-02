d3ed0c797a6df90a4c33c4cc9f75227b5848efa0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 02:26:57 +0000

    Added graphical orbit camera source file.

---
+++ src/cls/graphical/camera/orbit.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/camera/orbit.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace camera
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace camera
+    } // namespace graphical
+} // namespace arc
