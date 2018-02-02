24fb65487410158b10fe4f9cdbc0f9afe69b0c87
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 04:03:31 +0000

    Added graphical diffuse shader class header.

---
+++ src/cls/graphical/shader/diffuse.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_DIFFUSE_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_DIFFUSE_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/shader.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Specialisation of the shader class for ambient lighting.
+             */
+            class Diffuse : public Shader
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_DIFFUSE_HPP
