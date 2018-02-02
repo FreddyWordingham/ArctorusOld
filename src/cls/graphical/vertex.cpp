7e1d3fc318a5ad5239b980ea740e6ba8c4f35a2b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 02:44:16 +0000

    Added graphical vertex source file.

---
+++ src/cls/graphical/vertex.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/vertex.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace graphical
+} // namespace arc
