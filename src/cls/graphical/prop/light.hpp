2694d1144f75b73891fd2b7315bbdf30f005349e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 23:36:09 +0000

    Added graphical light prop class header.

---
+++ src/cls/graphical/prop/light.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_PROP_LIGHT_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_PROP_LIGHT_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/prop.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace prop
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Light prop.
+             */
+            class Light : public Prop
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace prop
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_PROP_LIGHT_HPP
