73b191a89c4ebc24b73332e3550467111f325fbe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 12:24:09 +0000

    Added normal graphical shader source file.

---
+++ src/cls/graphical/shader/normal.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   02/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/shader/normal.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
