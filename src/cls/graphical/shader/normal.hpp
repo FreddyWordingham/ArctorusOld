ddd0156ae0d412d72cf49aa2da09f5689431eac0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-02 23:55:10 +0000

    Added light_power uniform field.

--- src/cls/graphical/shader/normal.hpp
+++ src/cls/graphical/shader/normal.hpp
@@ -37,8 +37,9 @@ namespace arc
                 //  == FIELDS ==
               private:
                 //  -- Uniforms --
-                const GLint m_mvp_uni;  //! Model-view-projection uniform handle.
-                const GLint m_col_uni;  //! Colour uniform handle.
+                const GLint m_mvp_uni;      //! Model-view-projection uniform handle.
+                const GLint m_col_uni;      //! Colour uniform handle.
+                const GLint m_power_uni;    //! Light power uniform handle.
 
 
                 //  == INSTANTIATION ==
