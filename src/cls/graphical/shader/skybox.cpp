310cf3b1e69ff65b12148d685cc5f30a870538de
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-03 01:32:57 +0000

    Added graphical skybox shader source file.

---
+++ src/cls/graphical/shader/skybox.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   03/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/shader/skybox.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
