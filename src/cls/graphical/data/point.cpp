67d2cadec07170849af1dcc1d3675d2bae5224bf
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-03 18:31:12 +0000

    Added renderable graphical data source file.

---
+++ src/cls/graphical/data/point.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   03/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/graphical/data/point.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace data
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace data
+    } // namespace graphical
+} // namespace arc
