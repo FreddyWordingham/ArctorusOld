b90ebbfc3988f7f3579edd603a0dc8dedf45e757
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-03 01:32:30 +0000

    Added graphical skybox shader class header.

---
+++ src/cls/graphical/shader/skybox.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   03/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_SKYBOX_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_SKYBOX_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/shader.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Shader used to display skyboxes.
+             */
+            class Skybox : public Shader
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_SKYBOX_HPP
