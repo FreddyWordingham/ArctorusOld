a7c3d313ddd2218b9ae23af70a3fc5b814e4d1c0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-03 01:38:45 +0000

    Added constructor.

--- src/cls/graphical/shader/skybox.hpp
+++ src/cls/graphical/shader/skybox.hpp
@@ -45,6 +45,7 @@ namespace arc
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
+                Skybox(const std::string& t_vert_serial, const std::string& t_geom_serial, const std::string& t_frag_serial);
 
 
                 //  == METHODS ==
