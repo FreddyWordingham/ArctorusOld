ddfdf163d5dbcba687c0cf0dbfd26a14b775bff6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-03 18:30:34 +0000

    Added renderable graphical data class header.

---
+++ src/cls/graphical/data/point.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   03/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_DATA_POINT_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_DATA_POINT_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/data.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace data
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Data about a single renderable point.
+             */
+            class Point : public Data
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace data
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_DATA_POINT_HPP
