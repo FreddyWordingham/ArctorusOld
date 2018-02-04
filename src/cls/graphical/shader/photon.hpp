bf4bf48cb3fc03da37d1428646af48c91f45dd38
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-04 18:26:50 +0000

    Added photon graphical shader class header.

---
+++ src/cls/graphical/shader/photon.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   04/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_PHOTON_HPP
+#define ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_PHOTON_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/graphical/shader.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace graphical
+    {
+        namespace shader
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Photon packet path shader.
+             */
+            class Photon : public Shader
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace shader
+    } // namespace graphical
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_GRAPHICAL_SHADER_PHOTON_HPP
