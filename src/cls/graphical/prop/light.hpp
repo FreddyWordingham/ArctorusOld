efe60074fd50fa040b135e317823f4e56c7ef73f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-05 02:12:21 +0000

    Removed unneeded const qualifier.

--- src/cls/graphical/prop/light.hpp
+++ src/cls/graphical/prop/light.hpp
@@ -43,7 +43,7 @@ namespace arc
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                Light(const std::vector<Vertex>& t_vert, const glm::vec4& t_col, const float t_power);
+                Light(const std::vector<Vertex>& t_vert, const glm::vec4& t_col, float t_power);
 
 
                 //  == METHODS ==
