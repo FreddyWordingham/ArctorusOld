0b56453afd12ce352b1c9e6b66e15189c8036b70
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-05 13:58:00 +0000

    Added math general source.

---
+++ src/gen/math.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   05/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "gen/math.hpp"
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace gen
+} // namespace arc
