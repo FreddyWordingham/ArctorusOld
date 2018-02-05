275769b657dda55730ddc8a48fb8369e3f5abe8d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-05 00:12:05 +0000

    Removed unused geometry shader initialisation.

--- src/cls/graphical/shader/ambient.hpp
+++ src/cls/graphical/shader/ambient.hpp
@@ -45,7 +45,7 @@ namespace arc
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                Ambient(const std::string& t_vert_serial, const std::string& t_geom_serial, const std::string& t_frag_serial);
+                Ambient(const std::string& t_vert_serial, const std::string& t_frag_serial);
 
 
                 //  == METHODS ==
