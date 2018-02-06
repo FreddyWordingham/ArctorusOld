113274a9d74e909b53729e82ccbd8be7a30bd9a7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-06 14:56:11 +0000

    Added general rng source file.

---
+++ src/gen/rng.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   06/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "gen/rng.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace gen
+} // namespace arc
