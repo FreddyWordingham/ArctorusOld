a6b6af3f66621e762f6b9488b6a3633fc3ea569b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-06 14:57:15 +0000

    Moved gaussian function from inline to in source.

--- src/gen/rng.hpp
+++ src/gen/rng.hpp
@@ -37,7 +37,7 @@ namespace arc
 
         //  -- Generation --
         inline double random(double t_min = 0.0, double t_max = 1.0);
-        inline double gaussian(double t_mu = 0.0, double t_sigma = 1.0);
+        double gaussian(double t_mu = 0.0, double t_sigma = 1.0);
 
 
 
@@ -66,44 +66,6 @@ namespace arc
         inline double random(const double t_min, const double t_max)
         {
             return (random::Uniform::get_instance()(t_min, t_max));
-        }
-
-        /**
-         *  Generate a random double from a gaussian with a given average and standard deviation.
-         *
-         *  @param  t_mu    Average of the gaussian distribution.
-         *  @param  t_sigma Standard deviation of the distribution.
-         *
-         *  @return A random double from the specified gaussian distribution.
-         */
-        inline double gaussian(const double t_mu, const double t_sigma)
-        {
-            // Only generate a new pair of numbers every other call.
-            static bool generate = false;
-            generate = !generate;
-
-            // Store the second (spare) randomly generated value.
-            static double spare;
-
-            // If a new pair is not required, return the unused previously generated value.
-            if (!generate)
-            {
-                return ((spare * t_sigma) + t_mu);
-            }
-
-            // Generate a pair of uniform random values.
-            double r_0, r_1;
-            do
-            {
-                r_0 = random();
-                r_1 = random();
-            }
-            while (r_0 <= std::numeric_limits<double>::min());
-
-            // Convert the uniform random values to gaussian values.
-            spare = std::sqrt(-2.0 * std::log(r_0)) * sin(2.0 * M_PI * r_1);
-
-            return ((std::sqrt(-2.0 * std::log(r_0)) * cos(2.0 * M_PI * r_1) * t_sigma) + t_mu);
         }
 
 
