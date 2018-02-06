7ac71d005b48fdc1c0eb2444e6d22eba9b5389e4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-06 02:03:34 +0000

    Transformations appear to be out of order.

--- src/main.cpp
+++ src/main.cpp
@@ -36,27 +36,33 @@ int main()
 {
     LOG("Hello world!");
 
-/*    std::vector<graphical::point::Photon> path;
-    path.push_back(graphical::point::Photon({0.0f, 0.0f, 0.0f}, 400E-9f, 1.0f, 0.0));
-    path.push_back(graphical::point::Photon({0.0f, 0.0f, 1.0f}, 400E-9f, 1.0f, 1.0));
-    path.push_back(graphical::point::Photon({0.0f, 1.0f, 1.0f}, 400E-9f, 1.0f, 2.0));
-    path.push_back(graphical::point::Photon({2.0f, 1.0f, 1.0f}, 400E-9f, 1.0f, 4.0));
-    path.push_back(graphical::point::Photon({2.0f, 1.0f, 5.0f}, 400E-9f, 1.0f, 8.0));*/
-
     graphical::Scene scene;
+
+    std::vector<graphical::point::Photon> path_x;
+    path_x.push_back(graphical::point::Photon({0.0f, 0.0f, 0.0f}, 400E-9f, 1.0f, 0.0));
+    path_x.push_back(graphical::point::Photon({1.0f, 0.0f, 0.0f}, 400E-9f, 1.0f, 1.0));
+    scene.add_photon(path_x);
+
+    std::vector<graphical::point::Photon> path_y;
+    path_y.push_back(graphical::point::Photon({0.0f, 0.0f, 0.0f}, 400E-9f, 1.0f, 0.0));
+    path_y.push_back(graphical::point::Photon({0.0f, 1.0f, 0.0f}, 400E-9f, 1.0f, 1.0));
+    scene.add_photon(path_y);
+
+    std::vector<graphical::point::Photon> path_z;
+    path_z.push_back(graphical::point::Photon({0.0f, 0.0f, 0.0f}, 400E-9f, 1.0f, 0.0));
+    path_z.push_back(graphical::point::Photon({0.0f, 0.0f, 1.0f}, 400E-9f, 1.0f, 1.0));
+    scene.add_photon(path_z);
 
     geom::Mesh mesh(file::read("test/monkey.obj"));
 
-    const math::Mat<4, 4> pos_mat = math::create_pos_mat(math::Vec<3>({{-2.0, -2.0, -2.0}}), math::Vec<3>({{0.0, 0.0, -1.0}}),
-                                                         M_PI / 2.0, math::Vec<3>({{1.0, 2.0, 0.1}}));
-    const math::Mat<4, 4> dir_mat = math::create_dir_mat(math::Vec<3>({{0.0, 0.0, -1.0}}), M_PI / 2.0,
-                                                         math::Vec<3>({{1.0, 2.0, 0.1}}));
-
+    const math::Mat<4, 4> pos_mat = math::create_pos_mat(math::Vec<3>({{2.0, 2.0, 2.0}}), math::Vec<3>({{0.0, 0.0, -1.0}}), 0.0,
+                                                         math::Vec<3>({{0.1, 0.1, 0.1}}));
+    const math::Mat<4, 4> dir_mat = math::create_dir_mat(math::Vec<3>({{0.0, 0.0, -1.0}}), 0.0,
+                                                         math::Vec<3>({{0.1, 0.1, 0.1}}));
     mesh.transform(pos_mat, dir_mat);
+    scene.add_light(mesh, 2.0, {1.0f, 1.0f, 0.0f, 1.0f});
 
-
-//    scene.add_light(mesh, 2.0, {1.0f, 1.0f, 1.0f, 1.0f});
-    scene.add_entity(mesh, {0.9f, 0.0f, 0.0f, 1.0f});
+//    scene.add_entity(mesh, {0.9f, 0.0f, 0.0f, 1.0f});
 
     while (!scene.should_close())
     {
