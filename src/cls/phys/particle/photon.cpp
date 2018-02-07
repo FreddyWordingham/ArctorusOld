bbad1079128f6faa056af7373aca35ad1d605d14
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 12:17:08 +0000

    Added constructor.

--- src/cls/phys/particle/photon.cpp
+++ src/cls/phys/particle/photon.cpp
@@ -12,11 +12,6 @@
 
 
 
-//  == INCLUDES ==
-//  -- System --
-
-
-
 //  == NAMESPACE ==
 namespace arc
 {
@@ -29,10 +24,31 @@ namespace arc
 
             //  == INSTANTIATION ==
             //  -- Constructors --
-
-
-
-            //  -- Initialisation --
+            /**
+             *  Construct a photon packet at a given position with an initial direction at an emission time and
+             *  initial statistical weight and wavelength.
+             *
+             *  @param  t_pos           Initial position of the photon packet.
+             *  @param  t_dir           Initial direction of the photon packet.
+             *  @param  t_time          Emission time of the photon packet.
+             *  @param  t_wavelength    Initial wavelength of the photon packet.
+             *  @param  t_weight        Initial statistical weight of the photon packet.
+             *
+             *  @post   n_dir must be normalised.
+             *  @post   n_time must be positive.
+             *  @post   m_wavelength must be positive.
+             *  @post   n_weight must be positive.
+             */
+            Photon::Photon(const math::Vec<3>& t_pos, const math::Vec<3>& t_dir, const double t_time,
+                               const double t_wavelength, const double t_weight) :
+                Particle(t_pos, t_dir, t_time, t_weight),
+                m_wavelength(t_wavelength)
+            {
+                assert(n_dir.is_normalised());
+                assert(n_time > 0.0);
+                assert(m_wavelength > 0.0);
+                assert(n_weight > 0.0);
+            }
 
 
 
--- src/cls/phys/particle/photon.hpp
+++ src/cls/phys/particle/photon.hpp
@@ -43,13 +43,7 @@ namespace arc
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-
-              private:
-                //  -- Initialisation --
-
-
-                //  == OPERATORS ==
-              private:
+                Photon(const math::Vec<3>& t_pos, const math::Vec<3>& t_dir, double t_time, double t_wavelength, double t_weight = 1.0);
 
 
                 //  == METHODS ==
