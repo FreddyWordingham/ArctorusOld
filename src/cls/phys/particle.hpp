2d4b57a371538ef4e1615c03df7c80c0045f6f7a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 11:51:39 +0000

    Added the particle phys class.

---
+++ src/cls/phys/particle.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_PHYS_PARTICLE_HPP
+#define ARCTORUS_SRC_CLS_PHYS_PARTICLE_HPP
+
+
+
+//  == INCLUDES ==
+//  -- Classes --
+#include "cls/math/vec.hpp"
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A physical particle.
+         */
+        class Particle
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_PHYS_PARTICLE_HPP
