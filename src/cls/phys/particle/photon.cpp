7c3f825e4bd5b5822efa0e294e3d3b6841953a5e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 12:05:17 +0000

    Added photon particle phys source file.

---
+++ src/cls/phys/particle/photon.cpp
@@ -0,0 +1,41 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/phys/particle/photon.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+        namespace particle
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  -- Initialisation --
+
+
+
+        } // namespace particle
+    } // namespace phys
+} // namespace arc
