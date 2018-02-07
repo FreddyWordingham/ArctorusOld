3ed95e861b736220ade7e46906b266997499a632
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 02:59:22 +0000

    Cleaned up main.

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,6 @@ int main()
 {
     LOG("Hello world!");
 
-    auto mat = math::Mat<4, 4>({{{{0.0, 1.0, 2.0, 3.0}}, {{4.0, 5.0, 6.0, 7.0}}, {{8.0, 9.0, 10., 11.}}, {{12., 13., 14., 15.}}}});
-    VAL(mat);
-
     graphical::Scene scene;
 
     std::vector<graphical::point::Photon> path_x;
