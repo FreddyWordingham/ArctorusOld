69d5a77d8f7409bdbcb82c28e084a813174292c8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 12:04:57 +0000

    Added photon particle phys class header.

---
+++ src/cls/phys/particle/photon.hpp
@@ -0,0 +1,66 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_PHYS_PARTICLE_PHOTON_HPP
+#define ARCTORUS_SRC_CLS_PHYS_PARTICLE_PHOTON_HPP
+
+
+
+//  == BASE CLASS ==
+#include "cls/phys/particle.hpp"
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+        namespace particle
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Photon packet particle class.
+             */
+            class Photon : public Particle
+            {
+                //  == FIELDS ==
+              private:
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+              private:
+                //  -- Initialisation --
+
+
+                //  == OPERATORS ==
+              private:
+
+
+                //  == METHODS ==
+              private:
+            };
+
+
+
+        } // namespace particle
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_PHYS_PARTICLE_PHOTON_HPP
