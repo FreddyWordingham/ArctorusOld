e30813ece1665f89bdd794693acd2a514a90685c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-07 11:52:17 +0000

    Added the particle phys class source file.

---
+++ src/cls/phys/particle.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/phys/particle.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace phys
+} // namespace arc
