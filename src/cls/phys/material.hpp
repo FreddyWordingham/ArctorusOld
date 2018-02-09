851ba69cca707cf11cf5ce8b48a7948fa6d50f57
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 13:56:41 +0000

    Add the material physics class header.

---
+++ src/cls/phys/material.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_PHYS_MATERIAL_HPP
+#define ARCTORUS_SRC_CLS_PHYS_MATERIAL_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Material class used to store optical properties.
+         */
+        class Material
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_PHYS_MATERIAL_HPP
