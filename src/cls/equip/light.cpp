fbd6e3524ed6fd0bf68e8d41a2a28fcee0e4ed30
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 22:48:59 +0000

    Added the light equipment source file.

---
+++ src/cls/equip/light.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/equip/light.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace equip
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace equip
+} // namespace arc
