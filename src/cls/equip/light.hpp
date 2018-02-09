3cea74879876aa705cd10151d71b3b24496b07c6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 22:48:33 +0000

    Added the light equipment class header.

---
+++ src/cls/equip/light.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_EQUIP_LIGHT_HPP
+#define ARCTORUS_SRC_CLS_EQUIP_LIGHT_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace equip
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Light source class used to emit photons from.
+         */
+        class Light
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_EQUIP_LIGHT_HPP
