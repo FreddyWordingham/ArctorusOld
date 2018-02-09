6a88f6735166a262d496d1c59b71938a2f07e02c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 13:57:09 +0000

    Add the material physics source file.

---
+++ src/cls/phys/material.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/phys/material.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace phys
+} // namespace arc
