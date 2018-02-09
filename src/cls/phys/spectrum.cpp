3deea7b256c389c5b259eca37ff93e91d3392368
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 23:18:38 +0000

    Reworded post conditions.

--- src/cls/phys/spectrum.cpp
+++ src/cls/phys/spectrum.cpp
@@ -35,10 +35,11 @@ namespace arc
          *  @param  t_prob          Vector of corresponding probabilities.
          *
          *  @post   t_wavelength vector must be in ascending order.
-         *  @post   t_wavelength front element must be greater than zero.
-         *  @post   t_wavelength front element must be smaller than the back element.
          *  @post   t_wavelength vector must be the same size as t_prob vector.
          *  @post   t_prop vector must always be greater than zero.
+         *  @post   m_min_bound must be greater than zero.
+         *  @post   m_min_bound must be smaller than m_max_bound.
+
          */
         Spectrum::Spectrum(const std::vector<double>& t_wavelength, const std::vector<double>& t_prob) :
             m_min_bound(t_wavelength.front()),
@@ -46,10 +47,10 @@ namespace arc
             m_dist(t_wavelength, t_prob)
         {
             assert(utl::is_ascending(t_wavelength));
-            assert(t_wavelength.front() > 0.0);
-            assert(t_wavelength.front() < t_wavelength.back());
             assert(t_wavelength.size() == t_prob.size());
             assert(utl::is_always_greater_than(t_prob, 0.0));
+            assert(m_min_bound > 0.0);
+            assert(m_min_bound < m_max_bound);
         }
 
         /**
