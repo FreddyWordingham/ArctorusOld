c834bad685967cda8ba9715be15e1263fed2adb1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 23:43:42 +0000

    Added post condition.

--- src/cls/graphical/prop/light.cpp
+++ src/cls/graphical/prop/light.cpp
@@ -30,11 +30,14 @@ namespace arc
              *  @param  t_vert  Vector of vertices forming the prop.
              *  @param  t_power Power of the light.
              *  @param  t_col   Colour of the prop.
+             *
+             *  @post   t_power must be greater than zero.
              */
             Light::Light(const std::vector<Vertex>& t_vert, const float t_power, const glm::vec4& t_col) :
                 Prop(t_vert, t_col),
                 m_power(t_power)
             {
+                assert(t_power > 0.0);
             }
 
 
