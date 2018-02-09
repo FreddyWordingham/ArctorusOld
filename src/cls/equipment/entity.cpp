6447424b8ad5d16f38e155d0bb2dea5d3bca0565
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 20:54:56 +0000

    Added the entity source file.

---
+++ src/cls/equipment/entity.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "equip/entity.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace equip
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace equip
+} // namespace arc
