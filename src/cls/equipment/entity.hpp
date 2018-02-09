2185c769a9a14c5dd8f85469ff395ba33ceebbb2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 20:54:20 +0000

    Added the entity class header.

---
+++ src/cls/equipment/entity.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   09/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_EQUIPMENT_ENTITY_HPP
+#define ARCTORUS_SRC_CLS_EQUIPMENT_ENTITY_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace equipment
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Entity class coupling the optical properties of a material with the geometric boundaries of a mesh.
+         */
+        class Entity
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace equipment
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_EQUIPMENT_ENTITY_HPP
