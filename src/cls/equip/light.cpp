d859ee02dcec58d8d7f31242b86ffd48c573da79
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-09 23:45:52 +0000

    Added post-condition.

--- src/cls/equip/light.cpp
+++ src/cls/equip/light.cpp
@@ -28,12 +28,15 @@ namespace arc
          *  @param  t_mesh  Mesh to describe the surface of the light.
          *  @param  t_spec  Spectrum distribution.
          *  @param  t_power Power of the light source.
+         *
+         *  @post   m_power must be greater than zero.
          */
         Light::Light(const geom::Mesh& t_mesh, const phys::Spectrum& t_spec, const double t_power) :
             m_power(t_power),
             m_mesh(t_mesh),
             m_spec(t_spec)
         {
+            assert(m_power > 0.0);
         }
 
 
