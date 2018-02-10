6285de98e2fe5c318db3408a0da16540f24ba979
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-10 18:20:05 +0000

    Added conversion utility header file.

---
+++ src/utl/conversion.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   10/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_CONVERSION_HPP
+#define ARCTORUS_SRC_UTL_CONVERSION_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_CONVERSION_HPP
