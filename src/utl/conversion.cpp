5c59b907d3eda51f7d75ed36e031581f780dd6a1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-10 18:21:35 +0000

    Added conversion utility source file.

---
+++ src/utl/conversion.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   10/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "utl/conversion.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace utl
+} // namespace arc
