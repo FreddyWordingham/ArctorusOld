b32fde961c1f3a3b9cbbfc9a5b37220a984c657e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-10 00:44:16 +0000

    Added constants general header.

---
+++ src/gen/constants.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   10/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_CONSTANTS_HPP
+#define ARCTORUS_SRC_GEN_CONSTANTS_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+
+
+
+    //  == CONSTANTS ==
+
+
+
+
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_CONSTANTS_HPP
