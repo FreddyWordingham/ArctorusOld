d1cd461559d70a0f92f835fa086741481d7a9db5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-13 14:01:23 +0000

    Added step random generator source file.

---
+++ src/cls/random/step.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   13/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/random/step.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace random
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace random
+} // namespace arc
