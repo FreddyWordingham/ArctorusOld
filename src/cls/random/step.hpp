8eb25224a9563b8dee960772fc9417976998053f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-13 13:36:08 +0000

    Added step random generator class header.

---
+++ src/cls/random/step.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   13/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_RANDOM_STEP_HPP
+#define ARCTORUS_SRC_CLS_RANDOM_STEP_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace random
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A generator class which generates random numbers according to a given step probability distribution.
+         */
+        class Step
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace random
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_RANDOM_STEP_HPP
