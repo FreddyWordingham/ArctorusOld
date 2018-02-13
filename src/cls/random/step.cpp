ec24def595d09b58a9faf50116d1daebbeb40e33
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-13 14:39:58 +0000

    Added init_cdf method.

--- src/cls/random/step.cpp
+++ src/cls/random/step.cpp
@@ -34,7 +34,7 @@ namespace arc
          *  @param  t_x Vector of values.
          *  @param  t_p Vector of corresponding probabilities.
          *
-         *  @pre    m_p data must always be non-negative.
+         *  @post   m_p data must always be non-negative.
          */
         Step::Step(const std::vector<double>& t_x, const std::vector<double>& t_p) :
             m_x(t_x),
@@ -56,7 +56,7 @@ namespace arc
          *
          *  @return The initialised cumulative distribution frequency vector.
          */
-        std::vector<double> Step::init_cdf(const std::vector<double> &t_p) const
+        std::vector<double> Step::init_cdf(const std::vector<double>& t_p) const
         {
             assert(m_x.size() == t_p.size());
             assert(m_x.size() > 1);
@@ -64,6 +64,21 @@ namespace arc
 
             // Create return vector.
             std::vector<double> r_cdf(t_p.size());
+
+            // Initialise the cdf values.
+            r_cdf[0] = t_p[0];
+            for (size_t i = 1; i < t_p.size(); ++i)
+            {
+                r_cdf[i] = r_cdf[i - 1] + t_p[i];
+            }
+
+            // Normalise the cdf values.
+            for (size_t i = 0; i < t_p.size(); ++i)
+            {
+                r_cdf[i] /= r_cdf.back();
+            }
+
+            return (r_cdf);
         }
 
 
