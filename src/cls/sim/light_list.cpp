3e35e228162d43b637bf7c0fd2c0cff7abce2a69
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-14 17:25:26 +0000

    Added light list source file.

---
+++ src/cls/sim/light_list.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   14/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/sim/light_list.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace sim
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace sim
+} // namespace arc
