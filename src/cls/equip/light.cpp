a9a2dbd58aba79f8fa00d979d8a64559265cf2e5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-14 16:25:14 +0000

    Swapped parameters on photon constructor method call.

--- src/cls/equip/light.cpp
+++ src/cls/equip/light.cpp
@@ -79,7 +79,7 @@ namespace arc
             // Get a random position and normal from the mesh.
             const std::array<math::Vec<3>, 2> tri_pos_norm = m_mesh.get_tri(m_rand_tri()).get_random_pos_and_norm();
 
-            return (phys::particle::Photon(tri_pos_norm[0], tri_pos_norm[1], 0.0, 500e-9, 1.0, m_mat));
+            return (phys::particle::Photon(tri_pos_norm[0], tri_pos_norm[1], 0.0, 1.0, m_spec.gen_wavelength(), m_mat));
         }
 
 
