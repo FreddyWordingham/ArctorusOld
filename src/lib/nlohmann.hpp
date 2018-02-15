bebe463766f41d6e8edddd7dfbd7e3845ce9a378
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 01:37:10 +0000

    Added json nlohmann library header file.

---
+++ src/lib/nlohmann.hpp
@@ -0,0 +1,45 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ *
+ *  Copyright (c) 2018 University of Exeter All rights reserved.
+ */
+
+>>>>    Error!
+Header catagory
+looks  invalid
+!    <<<<
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_${ Catagory.toUpperCase() }_NLOHMANN_HPP
+#define ARCTORUS_SRC_${Catagory.toUpperCase()}_NLOHMANN_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace arc
+    {
+
+
+
+        //  == SETTINGS ==
+
+
+
+    } // namespace arc
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_${Catagory.toUpperCase()}_NLOHMANN_HPP
