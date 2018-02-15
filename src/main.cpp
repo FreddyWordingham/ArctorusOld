8b14ae5f7f5e03ad35543e4b4d667f7efb21cef4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 19:27:01 +0000

    Cleaned up main.

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
 {
     LOG("Hello world!");
 
-    setup::Sim pdt(file::read("../test/parameters.json"));
+    setup::Sim pdt(file::read("parameters.json"));
 
 /*    phys::Material mat(file::read("../test/intralipid_10.mat"));
     VAL(mat.get_ref_index(500e-9));
