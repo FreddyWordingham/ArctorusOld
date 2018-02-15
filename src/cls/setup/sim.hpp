78d870a90046481972c87d3d6cdeeeb3fe8f1cb2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 16:26:44 +0000

    Added simulation setup class.

---
+++ src/cls/setup/sim.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_SETUP_SIM_HPP
+#define ARCTORUS_SRC_CLS_SETUP_SIM_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace setup
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Simulation class which holds all objects required to run a simulation.
+         */
+        class Sim
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace setup
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_SETUP_SIM_HPP
