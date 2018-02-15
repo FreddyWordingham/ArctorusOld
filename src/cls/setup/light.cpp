17dd7f91510e03a891aeb0c618e7e52446878eb2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 15:39:22 +0000

    Added the light setup source file.

---
+++ src/cls/setup/light.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/setup/light.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace setup
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace setup
+} // namespace arc
