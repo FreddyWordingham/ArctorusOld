61c2bd295a691c5194aca8541c8729108e9c5d2f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 01:40:04 +0000

    Added json parser source file.

---
+++ src/cls/reader/json.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/reader/json.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace reader
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace reader
+} // namespace arc
