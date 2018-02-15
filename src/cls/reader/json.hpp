59e0034707d1da324c721ef0f6a94a492b27e06b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 01:39:40 +0000

    Added json parser class header.

---
+++ src/cls/reader/json.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_READER_JSON_HPP
+#define ARCTORUS_SRC_CLS_READER_JSON_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace reader
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  A reader class used to read data from json files.
+         */
+        class Json
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace reader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_READER_JSON_HPP
