d272ffd39cb57342f8235194f128a736f7a61405
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-15 15:39:01 +0000

    Added the light setup class header.

---
+++ src/cls/setup/light.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_SETUP_LIGHT_HPP
+#define ARCTORUS_SRC_CLS_SETUP_LIGHT_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace setup
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Store information required to setup a light equipment object.
+         */
+        class Light
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace setup
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_SETUP_LIGHT_HPP
