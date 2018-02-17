f2a90464d5c40faf57649bbe1ae7cef51276456c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-17 19:11:03 +0000

    Added light triangle directory list field.

--- src/cls/mesh/cell.hpp
+++ src/cls/mesh/cell.hpp
@@ -38,6 +38,7 @@ namespace arc
           private:
             //  -- Lists --
             const std::vector<std::array<size_t, 2>> m_entity_list; //! List of entity triangles with the cell.
+            const std::vector<std::array<size_t, 2>> m_light_list;  //! List of light triangles with the cell.
 
 
             //  == INSTANTIATION ==
