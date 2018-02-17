66f7490486d74546e3fe294e169ab5b36128b990
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-17 19:08:22 +0000

    Added mesh cell class header.

---
+++ src/cls/mesh/cell.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   17/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_MESH_CELL_HPP
+#define ARCTORUS_SRC_CLS_MESH_CELL_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace mesh
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Information about a single cell of the simulation.
+         */
+        class Cell
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace mesh
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_MESH_CELL_HPP
