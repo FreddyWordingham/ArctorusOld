241e99634ec19a8e60aff71bbf38fbd16808729c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-17 18:00:29 +0000

    Added the grid mesh class.

---
+++ src/cls/mesh/grid.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   17/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_MESH_GRID_HPP
+#define ARCTORUS_SRC_CLS_MESH_GRID_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace mesh
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Grid mesh class storing a three-dimensional array of uniform cells.
+         */
+        class Grid
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace mesh
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_MESH_GRID_HPP
