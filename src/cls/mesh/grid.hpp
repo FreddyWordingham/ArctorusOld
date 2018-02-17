23564efa6e66b0efae84327569cecb1da20ddef3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-17 18:04:36 +0000

    Added entity triangle list field.

--- src/cls/mesh/grid.hpp
+++ src/cls/mesh/grid.hpp
@@ -38,6 +38,9 @@ namespace arc
             const std::array<double, 3> m_min_bound;    //! Minimum bound of the cell.
             const std::array<double, 3> m_max_bound;    //! Maximum bound of the cell.
 
+            //  -- Triangles --
+            const std::vector<std::array<size_t, 2>> m_entity_tri;  //! List of entity triangles contained within the cell.
+
 
             //  == INSTANTIATION ==
           public:
