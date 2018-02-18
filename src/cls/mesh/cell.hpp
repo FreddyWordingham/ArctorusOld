3fa105ad07f53255761504d14d31d8f3a765d63f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-18 00:01:07 +0000

    Added bounds field.

--- src/cls/mesh/cell.hpp
+++ src/cls/mesh/cell.hpp
@@ -18,6 +18,9 @@
 #include <array>
 #include <vector>
 
+//  -- Classes --
+#include "cls/math/vec.hpp"
+
 
 
 //  == NAMESPACE ==
@@ -36,6 +39,10 @@ namespace arc
         {
             //  == FIELDS ==
           private:
+            //  -- Bounds --
+            const math::Vec<3> m_min_bound; //! Minimum bound of the cell.
+            const math::Vec<3> m_max_bound; //! Maximum bound of the cell.
+
             //  -- Lists --
             const std::vector<std::array<size_t, 2>> m_entity_list; //! List of entity triangles with the cell.
             const std::vector<std::array<size_t, 2>> m_light_list;  //! List of light triangles with the cell.
