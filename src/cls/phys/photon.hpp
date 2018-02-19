bf2201a4b851178aafff3c01c803e0c69f7d0e8f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-19 22:55:33 +0000

    Added photon packet class.

---
+++ src/cls/phys/photon.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   19/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_PHYS_PHOTON_HPP
+#define ARCTORUS_SRC_CLS_PHYS_PHOTON_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace phys
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Photon packet class.
+         */
+        class Photon
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_PHYS_PHOTON_HPP
