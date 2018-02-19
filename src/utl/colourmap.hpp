5c27134b8cd4a27c290827a9defeef0e03763773
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-19 12:18:03 +0000

    Added the colourmap utility header file.

---
+++ src/utl/colourmap.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   19/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_UTL_COLOURMAP_HPP
+#define ARCTORUS_SRC_UTL_COLOURMAP_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace colourmap
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace colourmap
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_UTL_COLOURMAP_HPP
