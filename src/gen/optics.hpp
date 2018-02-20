b9d2aa3e1bccd8b4cd9940079edb19739c6befa7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-20 16:24:20 +0000

    Added optics general header.

---
+++ src/gen/optics.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   20/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_GEN_OPTICS_HPP
+#define ARCTORUS_SRC_GEN_OPTICS_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace gen
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+    } // namespace gen
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_GEN_OPTICS_HPP
