bc7831f1e93896cb56909c688db14a7933540f3c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-20 20:17:16 +0000

    Removed diagnostic print.

--- src/gen/optics.cpp
+++ src/gen/optics.cpp
@@ -81,7 +81,6 @@ namespace arc
             const double       sin_a_t_sq = math::square(t_n) * (1.0 - math::square(cos_a_i));
             const math::Vec<3> r_out      = (t_in * t_n) + (t_norm * ((t_n * cos_a_i) - std::sqrt(1.0 - sin_a_t_sq)));
 
-            VAL(r_out.magnitude());
             assert(r_out.is_normalised());
 
             return (r_out);
