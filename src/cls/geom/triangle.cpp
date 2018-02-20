40eaee39746e9c4286fc33f97329facfe849ebfe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-20 01:35:44 +0000

    Improved init_norm method.

--- src/cls/geom/triangle.cpp
+++ src/cls/geom/triangle.cpp
@@ -52,11 +52,8 @@ namespace arc
         math::Vec<3> Triangle::init_norm() const
         {
             // Compute the cross-product of two edges to find the triangle's normal.
-            math::Vec<3> r_norm = (m_vert[BETA].get_pos() - m_vert[ALPHA].get_pos()) ^(m_vert[GAMMA].get_pos() - m_vert[ALPHA]
-                .get_pos());
-
-            // Normalise the normal.
-            r_norm.normalise();
+            math::Vec<3> r_norm = math::normalise(
+                (m_vert[BETA].get_pos() - m_vert[ALPHA].get_pos()) ^ (m_vert[GAMMA].get_pos() - m_vert[ALPHA].get_pos()));
 
             assert(r_norm.is_normalised());
 
