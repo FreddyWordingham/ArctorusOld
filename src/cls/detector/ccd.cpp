aaf4be3f8a34630434ab8f35c0a916d8e968ed93
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 16:43:50 +0000

    Added ccd mesh initialisation.

--- src/cls/detector/ccd.cpp
+++ src/cls/detector/ccd.cpp
@@ -12,11 +12,6 @@
 
 
 
-//  == INCLUDES ==
-//  -- System --
-
-
-
 //  == NAMESPACE ==
 namespace arc
 {
@@ -36,12 +31,11 @@ namespace arc
          *  @param  t_scale Vector of scaling values.
          */
         Ccd::Ccd(const math::Vec<3>& t_trans, const math::Vec<3>& t_dir, const double t_spin, const math::Vec<3>& t_scale) :
-            m_mesh("thing", t_trans, t_dir, t_spin, t_scale)
+            m_mesh(
+                "v 1.000000 1.000000 0.000000\nv -1.000000 1.000000 0.000000\nv 1.000000 -1.000000 0.000000\nv -1.000000 -1.000000 0.000000\nvn 0.0000 0.0000 1.0000\ns off\nf 2//1 3//1 1//1\nf 2//1 4//1 3//1",
+                t_trans, t_dir, t_spin, t_scale)
         {
         }
-
-
-        //  -- Initialisation --
 
 
 
