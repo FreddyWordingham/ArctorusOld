66f5356b7aa0847007686e398594426e9de9f7fb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 00:36:25 +0000

    Added data image source file.

---
+++ src/cls/data/image.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   21/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/data/image.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace data
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace data
+} // namespace arc
