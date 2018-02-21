6eb816707889e89c896831e1f2972e2f3052c30b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 16:31:32 +0000

    Added ccd detector class header.

---
+++ src/cls/detector/ccd.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   21/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_DETECTOR_CCD_HPP
+#define ARCTORUS_SRC_CLS_DETECTOR_CCD_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace detector
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  CCD like detector.
+         *  Rectangular in shape.
+         */
+        class Ccd
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace detector
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_DETECTOR_CCD_HPP
