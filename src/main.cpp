66d981f795f54feb5eac718c85300aebec178767
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 17:47:22 +0000

    Enabled ccd image saving.

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,9 @@ int main(const int t_argc, const char** t_argv)
     // Save grid data.
     pdt.save_grid_images(setup.parse_child<std::string>("output_dir"));
 
+    // Save ccd data.
+    pdt.save_ccd_images(setup.parse_child<std::string>("output_dir"));
+
     // Render the simulation scene.
     if (setup.parse_child<bool>("post_render", false))
     {
