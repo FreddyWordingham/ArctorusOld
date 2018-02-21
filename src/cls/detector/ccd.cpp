a94078fe9acb3d5d4a4c57402a481e4819a2ec24
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 16:31:54 +0000

    Added ccd detector source file.

---
+++ src/cls/detector/ccd.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   21/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/detector/ccd.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace detector
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace detector
+} // namespace arc
