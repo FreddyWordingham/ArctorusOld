51f5b4cb36db6ede47443776aa8d8203d73e3d57
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-21 00:36:09 +0000

    Added data image class header.

---
+++ src/cls/data/image.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   21/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_DATA_IMAGE_HPP
+#define ARCTORUS_SRC_CLS_DATA_IMAGE_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace data
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Image data writer.
+         */
+        class Image
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_DATA_IMAGE_HPP
