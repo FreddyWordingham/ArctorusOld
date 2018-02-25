e92e6fdb3aa52f25ad083935adbb27ba4579359a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-25 22:23:46 +0000

    Added name to constructor method.

--- src/cls/detector/ccd.cpp
+++ src/cls/detector/ccd.cpp
@@ -31,6 +31,7 @@ namespace arc
         /**
          *  Construct a ccd detector with a given number of pixels and a given transformation from a base square mesh.
          *
+         *  @param  t_name      Name of the ccd camera.
          *  @param  t_width     Width of the ccd in pixels.
          *  @param  t_height    Height of the ccd in pixels.
          *  @param  t_col       If true, save a coloured image.
@@ -39,8 +40,9 @@ namespace arc
          *  @param  t_spin      Spin angle.
          *  @param  t_scale     Vector of scaling values.
          */
-        Ccd::Ccd(const size_t t_width, const size_t t_height, const bool t_col, const math::Vec<3>& t_trans,
-                 const math::Vec<3>& t_dir, const double t_spin, const math::Vec<3>& t_scale) :
+        Ccd::Ccd(const std::string& t_name, const size_t t_width, const size_t t_height, const bool t_col,
+                 const math::Vec<3>& t_trans, const math::Vec<3>& t_dir, const double t_spin, const math::Vec<3>& t_scale) :
+            m_name(t_name),
             m_mesh(
                 "v 1.000000 1.000000 0.000000\nv -1.000000 1.000000 0.000000\nv 1.000000 -1.000000 0.000000\nv -1.000000 -1.000000 0.000000\nvn 0.0000 0.0000 1.0000\ns off\nf 2//1 3//1 1//1\nf 2//1 4//1 3//1",
                 t_trans, t_dir, t_spin, t_scale),
