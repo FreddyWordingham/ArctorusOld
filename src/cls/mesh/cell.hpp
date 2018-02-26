6a0a1904f9cec8021a48ed8f29cbd92cccd512d0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 02:48:27 +0000

    Added m_spectrometer_list field.

--- src/cls/mesh/cell.hpp
+++ src/cls/mesh/cell.hpp
@@ -47,9 +47,10 @@ namespace arc
             const math::Vec<3> m_max_bound; //! Maximum bound of the cell.
 
             //  -- Lists --
-            const std::vector<std::array<size_t, 2>> m_entity_list; //! List of entity triangles with the cell.
-            const std::vector<std::array<size_t, 2>> m_light_list;  //! List of light triangles with the cell.
-            const std::vector<std::array<size_t, 2>> m_ccd_list;    //! List of ccd triangles with the cell.
+            const std::vector<std::array<size_t, 2>> m_entity_list;         //! List of entity triangles with the cell.
+            const std::vector<std::array<size_t, 2>> m_light_list;          //! List of light triangles with the cell.
+            const std::vector<std::array<size_t, 2>> m_ccd_list;            //! List of ccd triangles with the cell.
+            const std::vector<std::array<size_t, 2>> m_spectrometer_list;   //! List of spectrometer triangles with the cell.
 
             //  -- Data --
             const bool m_empty;         //! True if the cell contains no triangles.
