19af811921e7d8bede900e138866864dec457576
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 18:04:40 +0000

    Added graphical module guard.

--- src/cls/graphical/shader.cpp
+++ src/cls/graphical/shader.cpp
@@ -7,6 +7,12 @@
 
 
 
+//  == MODULE ==
+#include "gen/config.hpp"
+#ifdef ENABLE_GRAPHICS
+
+
+
 //  == HEADER ==
 #include "cls/graphical/shader.hpp"
 
@@ -163,3 +169,8 @@ namespace arc
 
     } // namespace graphical
 } // namespace arc
+
+
+
+//  == MODULE END ==
+#endif
