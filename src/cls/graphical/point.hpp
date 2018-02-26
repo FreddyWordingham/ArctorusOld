082b06771201f160c952e0f0e53eb446c6c8555d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 18:06:48 +0000

    Added graphical module guard.

--- src/cls/graphical/point.hpp
+++ src/cls/graphical/point.hpp
@@ -7,6 +7,12 @@
 
 
 
+//  == MODULE ==
+#include "gen/config.hpp"
+#ifdef ENABLE_GRAPHICS
+
+
+
 //  == GUARD ==
 #ifndef ARCTORUS_SRC_CLS_GRAPHICAL_POINT_HPP
 #define ARCTORUS_SRC_CLS_GRAPHICAL_POINT_HPP
@@ -60,3 +66,8 @@ namespace arc
 
 //  == GUARD END ==
 #endif // ARCTORUS_SRC_CLS_GRAPHICAL_POINT_HPP
+
+
+
+//  == MODULE END ==
+#endif
