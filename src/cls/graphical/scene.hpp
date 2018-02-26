2aaa5e3b2e87fe5cbf19c57fa4e8c9311632a984
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 02:31:23 +0000

    Change entity hue range.

--- src/cls/graphical/scene.hpp
+++ src/cls/graphical/scene.hpp
@@ -79,8 +79,8 @@ namespace arc
         constexpr const float  PHOTON_TRAVEL_SPEED    = 1e-9f;  //! Photon travel speed when rendering path data.
         constexpr const double LIGHT_START_HUE        = 30.0;   //! Light prop start hue.
         constexpr const double LIGHT_END_HUE          = 60.0;   //! Light prop end hue.
-        constexpr const double ENTITY_START_HUE       = 240.0;  //! Entity prop start hue.
-        constexpr const double ENTITY_END_HUE         = 280.0;  //! Entity prop end hue.
+        constexpr const double ENTITY_START_HUE       = 300.0;  //! Entity prop start hue.
+        constexpr const double ENTITY_END_HUE         = 330.0;  //! Entity prop end hue.
         constexpr const double CCD_START_HUE          = 100.0;  //! Ccd prop start hue.
         constexpr const double CCD_END_HUE            = 140.0;  //! Ccd prop end hue.
         constexpr const double SPECTROMETER_START_HUE = 100.0;  //! Spectrometer prop start hue.
