beb04bd8c01b1558e70679c0b872b9abef4db724
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 18:06:05 +0000

    Added graphical module guard.

--- src/cls/graphical/prop.hpp
+++ src/cls/graphical/prop.hpp
@@ -7,6 +7,12 @@
 
 
 
+//  == MODULE ==
+#include "gen/config.hpp"
+#ifdef ENABLE_GRAPHICS
+
+
+
 //  == GUARD ==
 #ifndef ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
 #define ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
@@ -111,3 +117,8 @@ namespace arc
 
 //  == GUARD END ==
 #endif // ARCTORUS_SRC_CLS_GRAPHICAL_PROP_HPP
+
+
+
+//  == MODULE END ==
+#endif
