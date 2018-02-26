8585d4ce754063117776e6724643b5cbe6068c08
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 01:41:01 +0000

    Added spectrometer detector class header.

---
+++ src/cls/detector/spectrometer.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   26/02/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_DETECTOR_SPECTROMETER_HPP
+#define ARCTORUS_SRC_CLS_DETECTOR_SPECTROMETER_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace detector
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Spectrometer like detector.
+         *  Stores information about photons hitting a mesh surface.
+         */
+        class Spectrometer
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace detector
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_DETECTOR_SPECTROMETER_HPP
