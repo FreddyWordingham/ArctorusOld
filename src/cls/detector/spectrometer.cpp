fbb01d7cf8209aef9c6d6437b95df6ee380a3d04
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-26 01:41:53 +0000

    Added spectrometer detector class source file.

---
+++ src/cls/detector/spectrometer.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   26/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/detector/spectrometer.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace detector
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace detector
+} // namespace arc
