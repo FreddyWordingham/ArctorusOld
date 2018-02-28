24fe9590be9c5c59baad41fd71aa18d2faee1ad8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-28 18:52:37 +0000

    Decreased cell padding.

--- src/cls/graphical/scene.cpp
+++ src/cls/graphical/scene.cpp
@@ -368,7 +368,7 @@ namespace arc
                                 energy_density / max_energy_density);
 
                             // Add the cell prop.
-                            add_cell(cell.get_min_bound(), cell.get_max_bound(), 0.01,
+                            add_cell(cell.get_min_bound(), cell.get_max_bound(), 1e-6,
                                      {glm::vec3(col[R], col[G], col[B]), 1.0});
                         }
                     }
