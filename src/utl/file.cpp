3a4142dbaf84caf77d5bac40fe0546250fd05a4b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-02-28 21:58:19 +0000

    Added file utility source file.

---
+++ src/utl/file.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   28/02/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "utl/file.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace utl
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace utl
+} // namespace arc
