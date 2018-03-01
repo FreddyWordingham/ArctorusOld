843ba046b89b4ffaef91edf345efabe7ca1468bd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-01 20:11:39 +0000

    Added cons array of vecs field.

--- src/cls/geom/triangle.hpp
+++ src/cls/geom/triangle.hpp
@@ -42,6 +42,7 @@ namespace arc
             //  -- Vertices --
             const std::array<math::Vec<3>, 3> m_pos;    //! Vertex positions.
             const std::array<math::Vec<3>, 3> m_norm;   //! Vertex normals.
+            const std::array<math::Vec<3>, 3> m_cons;   //! Consistent normals used to calculate real reflections.
 
             //  -- Properties --
             const double       m_area;          //! Area of the triangle.
