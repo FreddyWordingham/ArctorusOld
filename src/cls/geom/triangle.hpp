5d93b40249146b485ddc92a418fc00cbbf15e3c0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-03 01:36:49 +0000

    Removed use flat and use phong settings.

--- src/cls/geom/triangle.hpp
+++ src/cls/geom/triangle.hpp
@@ -31,13 +31,6 @@ namespace arc
 
 
 
-        //  == SETTINGS ==
-        //  -- Optimisations --
-        const bool USE_FLAT  = false;   //! When true do not interpolate normals.
-        const bool USE_PHONG = true;    //! When true simplify normal interpolation to use phong normals.
-
-
-
         //  == CLASS ==
         /**
          *  Triangle class used to form triangular meshes.
