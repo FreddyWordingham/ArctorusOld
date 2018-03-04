e5054351a122d9ecfd10042cd661569f044488b7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-04 21:01:28 +0000

    Added grid_mutex field.

--- src/cls/setup/sim.hpp
+++ src/cls/setup/sim.hpp
@@ -87,8 +87,9 @@ namespace arc
 
 
             //  -- Threads --
-            std::mutex          m_ccd_mutex;            //! Protects the ccd objects.
-            std::mutex          m_spectrometer_mutex;   //! Protects the spectrometer objects.
+            std::mutex          m_ccd_mutex;            //! Protects the ccd objects data.
+            std::mutex          m_spectrometer_mutex;   //! Protects the spectrometer objects data.
+            std::mutex          m_grid_mutex;           //! Protects the grid data.
             std::vector<double> m_thread_progress;      //! Current progress of each thread.
             const double        m_log_update_period;    //! Period with which to update a progress print.
 
