57fa2ce85291f90ad5df8bd6f00f8549530a0582
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-04 20:53:54 +0000

    Removed copy constructor method.

--- src/cls/detector/ccd.hpp
+++ src/cls/detector/ccd.hpp
@@ -60,7 +60,6 @@ namespace arc
                          const math::Vec<3>& t_trans = math::Vec<3>(0.0, 0.0, 0.0),
                          const math::Vec<3>& t_dir = math::Vec<3>(0.0, 0.0, 1.0), double t_spin = 0.0,
                          const math::Vec<3>& t_scale = math::Vec<3>(1.0, 1.0, 1.0));
-            Ccd(Ccd&& t_ccd) noexcept;
 
 
             //  == METHODS ==
