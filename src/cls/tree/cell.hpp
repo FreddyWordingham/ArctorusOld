55c93329e174a867bd8c54ec14461a4111c08f7c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-07 00:02:28 +0000

    Added tree cell class header.

---
+++ src/cls/tree/cell.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/03/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_TREE_CELL_HPP
+#define ARCTORUS_SRC_CLS_TREE_CELL_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace tree
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Adaptive regular mesh tree forming octal cuboid cells.
+         */
+        class Cell
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace tree
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_TREE_CELL_HPP
