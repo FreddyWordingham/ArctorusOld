39817e2b87eef5bf8ff84052d9368836f0f98b22
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-07 00:02:48 +0000

    Added tree cell source file.

---
+++ src/cls/tree/cell.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/03/2018.
+ */
+
+
+
+//  == HEADER ==
+#include "cls/tree/cell.hpp"
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace tree
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  -- Initialisation --
+
+
+
+    } // namespace tree
+} // namespace arc
