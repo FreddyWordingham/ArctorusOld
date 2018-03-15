2a444f6317af23432a9c2d8c8902c49fce03e5a6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-15 16:49:47 +0000

    Added the dynamic histogram class.

---
+++ src/cls/data/dynamicHistogram.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/03/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_DATA_DYNAMICHISTOGRAM_HPP
+#define ARCTORUS_SRC_CLS_DATA_DYNAMICHISTOGRAM_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace data
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Collects double values and bins them into an array of bins with dynamic limits.
+         */
+        class DynamicHistogram
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_DATA_DYNAMICHISTOGRAM_HPP
