4f2b345cf686aa5f698c4e193825886ba35c8ce4
Laura Moran <lm579@exeter.ac.uk>
2018-03-16 13:50:40 +0000

    Added pixel intensity plot and average radial intensity plot to python folder.

---
+++ src/gen/config.hpp
@@ -0,0 +1,52 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   07/01/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_RES_SRC_GEN_CONFIG_HPP_IN
+#define ARCTORUS_RES_SRC_GEN_CONFIG_HPP_IN
+
+
+
+//  == OPTIONS ==
+//  -- Compile-Time --
+// Coloured log is disabled.
+// Verbose log is disabled.
+// Graphics are disabled.
+// Photon paths are disabled.
+
+#define ENABLE_LOG_COLOUR
+#define ENABLE_LOG_VERBOSE
+#define ENABLE_GRAPHICS
+#define ENABLE_PHOTON_PATHS
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace config
+    {
+
+
+
+        //  == SETTINGS ==
+        //  -- Build Info --
+        constexpr const char* ARCTORUS_DIR = "/Users/lm579/Arctorus/";     //! Arctorus directory path.
+        constexpr const char* BUILD_INFO   = "Raman-48959794-2722"
+            " : Clang : release : 2018-03-05";    //! Arctorus build information.
+
+
+
+    } // namespace config
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_RES_SRC_GEN_CONFIG_HPP_IN
