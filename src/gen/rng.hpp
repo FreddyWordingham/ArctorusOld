9f4edb3bed30dc48c8d7685b0a17384777d485da
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-16 16:16:54 +0000

    Re-added rng generator functions.

--- src/gen/rng.hpp
+++ src/gen/rng.hpp
@@ -36,6 +36,7 @@ namespace arc
         inline void seed(random::Uniform::base t_seed = static_cast<random::Uniform::base>(time(nullptr)));
 
         //  -- Generation --
+        inline double random(double t_min = 0.0, double t_max = 1.0);
         double henyey_greenstein(double t_g);
         double gaussian(double t_mu = 0.0, double t_sigma = 1.0);
 
@@ -51,6 +52,21 @@ namespace arc
         inline void seed(const random::Uniform::base t_seed)
         {
             random::Uniform::get_instance(t_seed);
+        }
+
+
+        //  -- Generation --
+        /**
+         *  Retrieve a random number from the uniform generator.
+         *
+         *  @param  t_min   Minimum bound for the generated value.
+         *  @param  t_max   Maximum bound for the generated value.
+         *
+         *  @return A random double between the given bounds.
+         */
+        inline double random(const double t_min, const double t_max)
+        {
+            return (random::Uniform::get_instance().gen_value(t_min, t_max));
         }
 
 
