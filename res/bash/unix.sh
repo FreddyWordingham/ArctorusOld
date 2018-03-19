e205279eb225451cf6441aeceb70d0d0b72fa1cf
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 17:53:12 +0000

    Added the clean function.

--- res/bash/unix.sh
+++ res/bash/unix.sh
@@ -4,3 +4,21 @@ alias arctorus=$ARCTORUS_DIR/bin/arctorus
 
 
 #   == FUNCTIONS ==
+#   -- Cleaning --
+#
+#   Remove temporary Arctorus build files.
+#
+clean_arc()
+{
+    # Change directory.
+    cd $ARCTORUS_DIR > /dev/null;
+
+    # Remove files.
+    rm -r bin/*;
+    rm -r build/debug/*;
+    rm -r build/release/*;
+    rm src/hdr/config.hpp;
+
+    # Change to previous directory.
+    cd - > /dev/null;
+}
