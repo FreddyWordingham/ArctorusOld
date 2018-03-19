c227f57a799083596fa2f93e56120184ee5c1436
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 18:07:18 +0000

    Added make_arc function.

--- res/bash/unix.sh
+++ res/bash/unix.sh
@@ -33,18 +33,41 @@ build_arc()
     # Check number of command line arguments.
     if [ "$#" -ne 7 ]; then
         echo "Invalid build call:";
-        echo "build <type> <c compiler> <c++ compiler> <colour> <verbose> <graphics> <photon paths>";
+        echo "build_arc <type> <c compiler> <c++ compiler> <colour> <verbose> <graphics> <photon paths>";
         return;
     fi
 
     # Change directory.
-    cd $ARCTORUS_DIR/build/release > /dev/null;
+    cd $ARCTORUS_DIR/build/$1 > /dev/null;
 
     # Clean.
     clean;
 
     # Run cmake.
     cmake -j 4 -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_COMPILER=$2 -DCMAKE_CXX_COMPILER=$3 -DLOG_COLOUR=$4 -DLOG_VERBOSE=$5 -DGRAPHICS=$6 -DPHOTON_PATHS=$7 ../..;
+
+    # Change to previous directory.
+    cd - > /dev/null;
+}
+
+#
+#   Re-make Arctorus.
+#   Previous build files are deleted prior to building.
+#
+make_arc()
+{
+    # Check number of command line arguments.
+    if [ "$#" -ne 1 ]; then
+        echo "Invalid make call:";
+        echo "make_arc <type>";
+        return;
+    fi
+
+    # Change directory.
+    cd $ARCTORUS_DIR/build/$1 > /dev/null;
+
+    # Remove arctorus binary.
+    rm $ARCTORUS_DIR/bin/arctorus;
 
     # Run make.
     make -j 4;
