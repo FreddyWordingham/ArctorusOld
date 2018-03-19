dce4d0c4ff1f64b663809677541a4feb87eb2857
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 18:28:52 +0000

    Added the vec3 math class header.

---
+++ src/cls/math/vec3.hpp
@@ -0,0 +1,65 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   19/03/2018.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTORUS_SRC_CLS_MATH_VEC3_HPP
+#define ARCTORUS_SRC_CLS_MATH_VEC3_HPP
+
+
+
+//  == INCLUDES ==
+//  -- System --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace math
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Three-dimensional mathematical vector class.
+         *  Acts as a mathematical vector of double values.
+         *  Should not be used for general purpose storage.
+         */
+        class Vec3
+        {
+            //  == FIELDS ==
+          private:
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+          private:
+            //  -- Initialisation --
+
+
+            //  == OPERATORS ==
+          private:
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTORUS_SRC_CLS_MATH_VEC3_HPP
