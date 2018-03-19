29070060bc632d0abddefe89695239c575253bb3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 17:51:56 +0000

    Added move directory function.

--- res/bash/unix.sh
+++ res/bash/unix.sh
@@ -1,2 +1,15 @@
 #   == ALIASES ==
 alias arctorus=$ARCTORUS_DIR/bin/arctorus
+
+
+
+#   == FUNCTIONS ==
+#   -- Change Directory --
+#
+#   Move to the Arctorus top directory.
+#
+xcd()
+{
+    cd $ARCTORUS_DIR;
+    ls -l;
+}
