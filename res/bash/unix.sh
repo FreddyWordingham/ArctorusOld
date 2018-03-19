3b36c3f79b895c91fb4da8a3b2eb7f5fab9edf19
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 18:03:47 +0000

    Improved build function.

--- res/bash/unix.sh
+++ res/bash/unix.sh
@@ -1,5 +1,5 @@
 #   == ALIASES ==
-alias arctorus=$ARCTORUS_DIR/bin/arctorus
+alias arc=$ARCTORUS_DIR/bin/arctorus
 
 
 
@@ -18,6 +18,36 @@ clean_arc()
     rm -r build/debug/*;
     rm -r build/release/*;
     rm src/hdr/config.hpp;
+
+    # Change to previous directory.
+    cd - > /dev/null;
+}
+
+#   -- Building --
+#
+#   Build Arctorus.
+#   Previous build files are deleted prior to building.
+#
+build()
+{
+    # Check number of command line arguments.
+    if [ "$#" -ne 7 ]; then
+        echo "Invalid build call:";
+        echo "build <type> <c compiler> <c++ compiler> <colour> <verbose> <graphics> <photon paths>";
+        return;
+    fi
+
+    # Change directory.
+    cd $ARCTORUS_DIR/build/release > /dev/null;
+
+    # Clean.
+    clean;
+
+    # Run cmake.
+    cmake -j 4 -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_COMPILER=$2 -DCMAKE_CXX_COMPILER=$3 -DLOG_COLOUR=$4 -DLOG_VERBOSE=$5 -DGRAPHICS=$6 -DPHOTON_PATHS=$7 ../..;
+
+    # Run make.
+    make -j 4;
 
     # Change to previous directory.
     cd - > /dev/null;
