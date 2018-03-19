6d8c1c2b43efbd0d371246ea284d44172b58bc3b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-03-19 17:52:27 +0000

    Removed change directory function.

--- res/bash/unix.sh
+++ res/bash/unix.sh
@@ -4,12 +4,3 @@ alias arctorus=$ARCTORUS_DIR/bin/arctorus
 
 
 #   == FUNCTIONS ==
-#   -- Change Directory --
-#
-#   Move to the Arctorus top directory.
-#
-xcd()
-{
-    cd $ARCTORUS_DIR;
-    ls -l;
-}
